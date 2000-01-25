2a1a929aec185beb1c8a89ddf0e304e19b07427b
People doing a 16 bpc version of gimp <film@src.gnome.org>
2000-01-25 16:32:00 +0000

    Tue Jan 25 10:51:05 EST 2000 calvinw@mindspring.com
        *More details for GeglDrawable class.
    
        *Fixed a bunch of gegl_destroys to be correct.
    
        *Straightened out some of the forward header problems
         by updating to gob-0.92.2. No gob patch is necessary
         now.

--- gegl/testgegl.c
+++ gegl/testgegl.c
@@ -4,6 +4,8 @@
 
 #include "gegl-color.h"
 #include "gegl-color-model-rgb-float.h"
+#include "gegl-drawable.h"
+#include "gegl-graphics-state.h"
 #include "gegl-image-buffer.h"
 #include "gegl-fill-op.h"
 #include "gegl-print-op.h"
@@ -17,7 +19,43 @@ main (int argc, char *argv[])
 {  
   gtk_init (&argc, &argv);
 
-#if 1 
+#if 0      /*This uses the GeglDrawable class */ 
+  {
+
+	GeglColorModel *cm = GEGL_COLOR_MODEL(gegl_color_model_rgb_float_new(FALSE,FALSE));
+	GeglDrawable *d = GEGL_DRAWABLE(gegl_drawable_new(cm,"myDrawable",5,5));
+	GeglGraphicsState *state = gegl_drawable_get_graphics_state (d);
+	GeglRect r;
+
+	gegl_rect_set (&r, 0,0,5,5);
+        gegl_color_set_constant (state->fg_color, COLOR_RED);
+        gegl_drawable_fill(d, &r); 
+
+	gegl_rect_set (&r, 1,1,4,4);
+        gegl_color_set_constant (state->fg_color, COLOR_GREEN);
+        gegl_drawable_fill(d, &r); 
+
+
+        /* Print out the image values using the 
+	   print operator 
+        */
+        {
+          GeglOp *op;
+	  GeglRect r;
+	  gegl_rect_set (&r, 0,0,5,5);
+	  op = GEGL_OP (gegl_print_op_new (gegl_drawable_get_image_buffer(d), &r));
+          gegl_op_apply (op);
+	  gegl_object_destroy (GEGL_OBJECT(op)); 
+
+	}
+
+	gegl_object_destroy (GEGL_OBJECT(d)); 
+	gegl_object_destroy (GEGL_OBJECT(cm)); 
+
+  }
+#endif
+
+#if 1   /* This one uses the GeglImageBuffer and GeglOp directly */ 
   {
 
 	GeglColorModel *cm = GEGL_COLOR_MODEL(
