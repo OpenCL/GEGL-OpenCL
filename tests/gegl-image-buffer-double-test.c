0fd8267bc6ed424615d1bcf38bd6032edee2d0ae
Daniel Rogers <dsrogers@src.gnome.org>
2003-08-25 23:18:26 +0000

    More complete tests of GeglBufferDouble, including the get and set element
    More complete tests of GeglBufferDouble, including the get and set
    element functions.

--- tests/gegl-image-buffer-double-test.c
+++ tests/gegl-image-buffer-double-test.c
@@ -7,27 +7,72 @@
 static void
 test_buffer_double_g_object_new(Test *test) {
 	GeglBuffer* buffer_double=g_object_new(GEGL_TYPE_BUFFER_DOUBLE,
-														 "num_banks",1,
-														 "elements_per_bank",400,
-														 NULL);
+					       "num_banks",1,
+					       "elements_per_bank",400,
+					       NULL);
 	ct_test(test,buffer_double!=NULL);
 	ct_test(test, GEGL_IS_BUFFER_DOUBLE(buffer_double));
-    ct_test(test, g_type_parent(GEGL_TYPE_BUFFER_DOUBLE) == GEGL_TYPE_BUFFER);
-    ct_test(test, !strcmp("GeglBufferDouble", g_type_name(GEGL_TYPE_BUFFER_DOUBLE)));
-
-    g_object_unref(buffer_double);
+	ct_test(test, g_type_parent(GEGL_TYPE_BUFFER_DOUBLE) == GEGL_TYPE_BUFFER);
+	ct_test(test, !strcmp("GeglBufferDouble", g_type_name(GEGL_TYPE_BUFFER_DOUBLE)));
+	
+	g_object_unref(buffer_double);
 }
 
-/*
+
 static void
 test_buffer_double_properties(Test* test) {
     GeglBufferDouble* buffer_double=g_object_new(GEGL_TYPE_BUFFER_DOUBLE,
                                                  "num_banks", 42,
                                                  "elements_per_bank", 47,
                                                  NULL);
-    //TODO
+    GObject* object=G_OBJECT(buffer_double);
+    gint num_banks;
+    gint elements_per_bank;
+    g_object_get(object, "num_banks",&num_banks,NULL);
+    ct_test(test, num_banks == 42);
+    
+    g_object_get(object, "elements_per_bank",&elements_per_bank,NULL);
+    ct_test(test, elements_per_bank == 47);
+    
+    g_object_unref(buffer_double);
 }
-*/
+
+static void
+test_buffer_double_get_set(Test* test) {
+  GeglBufferDouble* buffer_double=g_object_new(GEGL_TYPE_BUFFER_DOUBLE,
+					       "num_banks", 10,
+					       "elements_per_bank", 10,
+					       NULL);
+  GeglBuffer* buffer=GEGL_BUFFER(buffer_double);
+  gint bank;
+  gint element;
+
+  for (bank=0;bank<10;bank++) {
+    for (element=0;element<10;element++) {
+      gegl_buffer_set_element_double(buffer,bank,element,bank+element);
+    }
+  }
+
+  gboolean all_ok=TRUE;
+
+  for (bank=0;bank<10;bank++) {
+    for (element=0;element<10;element++) {
+      if (gegl_buffer_get_element_double(buffer,bank,element) !=
+	  bank+element) {
+	all_ok=FALSE;
+	break;
+      }
+    }
+    if (all_ok==FALSE) {
+      break;
+    }
+  }
+  
+  ct_test(test,all_ok==TRUE);
+
+  g_object_unref(buffer_double);
+}
+
 static void
 test_buffer_double_factory(Test* test) {
     GeglBufferDouble* buffer_double=
@@ -43,12 +88,13 @@ test_buffer_double_factory(Test* test) {
 Test *
 create_buffer_double_test()
 {
-  	Test* t = ct_create("GeglBufferDoubleTest");
-
+  Test* t = ct_create("GeglBufferDoubleTest");
+  
 //  g_assert(ct_addSetUp(t, color_test_setup));
 //  g_assert(ct_addTearDown(t, color_test_teardown));
-  	g_assert(ct_addTestFun(t, test_buffer_double_g_object_new));
-    g_assert(ct_addTestFun(t, test_buffer_double_factory));
-    
-  	return t; 
+  g_assert(ct_addTestFun(t, test_buffer_double_g_object_new));
+  g_assert(ct_addTestFun(t, test_buffer_double_factory));
+  g_assert(ct_addTestFun(t, test_buffer_double_properties));
+  g_assert(ct_addTestFun(t, test_buffer_double_get_set));
+  return t; 
 }
