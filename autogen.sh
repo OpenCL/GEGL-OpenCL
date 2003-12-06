88837e26a6f547ed620272abaebc55278c8245c7
09:04 Daniel Rogers <daniel@phasevelocity.org>
2003-12-06 17:06:13 +0000

    autogen.sh Updated autogen.sh to work with newer libtool/autofoo stuff.
    2003-12-06 09:04 Daniel Rogers <daniel@phasevelocity.org>
    	* autogen.sh
    	Updated autogen.sh to work with newer libtool/autofoo stuff.

--- autogen.sh
+++ autogen.sh
@@ -1,87 +1,204 @@
-#!/bin/sh
-# Run this to generate all the initial makefiles, etc.
+#!/bin/sh 
+
+# This script does all the magic calls to automake/autoconf and
+# friends that are needed to configure a cvs checkout.  As described in
+# the file HACKING you need a couple of extra tools to run this script
+# successfully.
+#
+# If you are compiling from a released tarball you don't need these
+# tools and you shouldn't use this script.  Just call ./configure
+# directly.
+
+
+PROJECT="The GIMP"
+TEST_TYPE=-d
+FILE=plug-ins
+
+LIBTOOL_REQUIRED_VERSION=1.3.4
+LIBTOOL_WIN32=1.5
+AUTOCONF_REQUIRED_VERSION=2.54
+AUTOMAKE_REQUIRED_VERSION=1.6
+GLIB_REQUIRED_VERSION=2.0.0
+INTLTOOL_REQUIRED_VERSION=0.17
+
 
 srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
-
 ORIGDIR=`pwd`
 cd $srcdir
-PROJECT=Gegl
-TEST_TYPE=-f
-FILE=gegl/gegl-object.h
+
+
+check_version ()
+{
+    if expr $1 \>= $2 > /dev/null; then
+	echo "yes (version $1)"
+    else
+	echo "Too old (found version $1)!"
+	DIE=1
+    fi
+}
+
+echo
+echo "I am testing that you have the required versions of libtool, autoconf," 
+echo "automake, glib-gettextize and intltoolize. This test is not foolproof,"
+echo "so if anything goes wrong, see the file HACKING for more information..."
+echo
 
 DIE=0
 
-have_libtool=false
-if libtool --version < /dev/null > /dev/null 2>&1 ; then
-	libtool_version=`libtoolize --version |  libtoolize --version | sed 's/^[^0-9]*\([0-9.][0-9.]*\).*/\1/'`
-	case $libtool_version in
-	    1.4*)
-		have_libtool=true
-		;;
-	esac
-fi
-if $have_libtool ; then : ; else
+
+OS=`uname -s`
+case $OS in 
+    *YGWIN* | *INGW*)
+	echo "Looks like Win32, you will need libtool $LIBTOOL_WIN32 or newer."
 	echo
-	echo "You must have libtool 1.4 installed to compile $PROJECT."
-	echo "Install the appropriate package for your distribution,"
-	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
-	DIE=1
+	LIBTOOL_REQUIRED_VERSION=$LIBTOOL_WIN32
+	;;
+esac
+
+
+echo -n "checking for libtool >= $LIBTOOL_REQUIRED_VERSION ... "
+if (libtoolize --version) < /dev/null > /dev/null 2>&1; then
+    VER=`libtoolize --version \
+         | grep libtool | sed "s/.* \([0-9.]*\)[-a-z0-9]*$/\1/"`
+    check_version $VER $LIBTOOL_REQUIRED_VERSION
+else
+    echo
+    echo "  You must have libtool installed to compile $PROJECT."
+    echo "  Install the appropriate package for your distribution,"
+    echo "  or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+    DIE=1;
 fi
 
-(autoconf --version) < /dev/null > /dev/null 2>&1 || {
-	echo
-	echo "You must have autoconf installed to compile $PROJECT."
-	echo "libtool the appropriate package for your distribution,"
-	echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
-	DIE=1
-}
-
-have_automake=false
-if automake --version < /dev/null > /dev/null 2>&1 ; then
-	automake_version=`automake --version | grep 'automake (GNU automake)' | sed 's/^[^0-9]*\(.*\)/\1/'`
-	case $automake_version in
-	   1.2*|1.3*|1.4) 
-		;;
-	   *)
-		have_automake=true
-		;;
-	esac
+echo -n "checking for autoconf >= $AUTOCONF_REQUIRED_VERSION ... "
+if (autoconf --version) < /dev/null > /dev/null 2>&1; then
+    VER=`autoconf --version \
+         | grep -iw autoconf | sed "s/.* \([0-9.]*\)[-a-z0-9]*$/\1/"`
+    check_version $VER $AUTOCONF_REQUIRED_VERSION
+else
+    echo
+    echo "  You must have autoconf installed to compile $PROJECT."
+    echo "  Download the appropriate package for your distribution,"
+    echo "  or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
+    DIE=1;
 fi
-if $have_automake ; then : ; else
-	echo
-	echo "You must have automake 1.4-p1 installed to compile $PROJECT."
-	echo "Get ftp://ftp.gnu.org/pub/gnu/automake/automake-1.4-p1.tar.gz"
-	echo "(or a newer version if it is available)"
-	DIE=1
+
+echo -n "checking for automake >= $AUTOMAKE_REQUIRED_VERSION ... "
+if (automake-1.7 --version) < /dev/null > /dev/null 2>&1; then
+   AUTOMAKE=automake-1.7
+   ACLOCAL=aclocal-1.7
+elif (automake-1.6 --version) < /dev/null > /dev/null 2>&1; then
+   AUTOMAKE=automake-1.6
+   ACLOCAL=aclocal-1.6
+else
+    echo
+    echo "  You must have automake 1.6 or 1.7 installed to compile $PROJECT."
+    echo "  Get ftp://ftp.gnu.org/pub/gnu/automake/automake-1.7.3.tar.gz"
+    echo "  (or a newer version if it is available)"
+    DIE=1
+fi
+
+if test x$AUTOMAKE != x; then
+    VER=`$AUTOMAKE --version \
+         | grep automake | sed "s/.* \([0-9.]*\)[-a-z0-9]*$/\1/"`
+    check_version $VER $AUTOMAKE_REQUIRED_VERSION
+fi
+
+echo -n "checking for glib-gettextize >= $GLIB_REQUIRED_VERSION ... "
+if (glib-gettextize --version) < /dev/null > /dev/null 2>&1; then
+    VER=`glib-gettextize --version \
+         | grep glib-gettextize | sed "s/.* \([0-9.]*\)/\1/"`
+    check_version $VER $GLIB_REQUIRED_VERSION
+else
+    echo
+    echo "  You must have glib-gettextize installed to compile $PROJECT."
+    echo "  glib-gettextize is part of glib-2.0, so you should already"
+    echo "  have it. Make sure it is in your PATH."
+    DIE=1
+fi
+
+echo -n "checking for intltool >= $INTLTOOL_REQUIRED_VERSION ... "
+if (intltoolize --version) < /dev/null > /dev/null 2>&1; then
+    VER=`intltoolize --version \
+         | grep intltoolize | sed "s/.* \([0-9.]*\)/\1/"`
+    check_version $VER $INTLTOOL_REQUIRED_VERSION
+else
+    echo
+    echo "  You must have intltool installed to compile $PROJECT."
+    echo "  Get the latest version from"
+    echo "  ftp://ftp.gnome.org/pub/GNOME/sources/intltool/"
+    DIE=1
 fi
 
 if test "$DIE" -eq 1; then
-	exit 1
+    echo
+    echo "Please install/upgrade the missing tools and call me again."
+    echo	
+    exit 1
 fi
+
 
 test $TEST_TYPE $FILE || {
-	echo "You must run this script in the top-level $PROJECT directory"
-	exit 1
+    echo
+    echo "You must run this script in the top-level $PROJECT directory."
+    echo
+    exit 1
 }
 
-if test -z "$AUTOGEN_SUBDIR_MODE"; then
-        if test -z "$*"; then
-                echo "I am going to run ./configure with no arguments - if you wish "
-                echo "to pass any to it, please specify them on the $0 command line."
-        fi
+
+if test -z "$*"; then
+    echo
+    echo "I am going to run ./configure with no arguments - if you wish "
+    echo "to pass any to it, please specify them on the $0 command line."
+    echo
 fi
 
-aclocal $ACLOCAL_FLAGS
+if test -z "$ACLOCAL_FLAGS"; then
 
-libtoolize --copy --force
+    acdir=`$ACLOCAL --print-ac-dir`
+    m4list="glib-2.0.m4 glib-gettext.m4 gtk-2.0.m4 intltool.m4 pkg.m4"
 
-automake --add-missing $am_opt
-autoconf
+    for file in $m4list
+    do
+	if [ ! -f "$acdir/$file" ]; then
+	    echo
+	    echo "WARNING: aclocal's directory is $acdir, but..."
+            echo "         no file $acdir/$file"
+            echo "         You may see fatal macro warnings below."
+            echo "         If these files are installed in /some/dir, set the ACLOCAL_FLAGS "
+            echo "         environment variable to \"-I /some/dir\", or install"
+            echo "         $acdir/$file."
+            echo
+        fi
+    done
+fi
+
+$ACLOCAL $ACLOCAL_FLAGS
+RC=$?
+if test $RC -ne 0; then
+   echo "$ACLOCAL gave errors. Please fix the error conditions and try again."
+   exit 1
+fi
+
+libtoolize --force || exit 1
+
+# optionally feature autoheader
+(autoheader --version)  < /dev/null > /dev/null 2>&1 && autoheader || exit 1
+
+$AUTOMAKE --add-missing || exit 1
+autoconf || exit 1
+
+glib-gettextize --copy --force || exit 1
+intltoolize --copy --force --automake || exit 1
+
 cd $ORIGDIR
 
-if test -z "$AUTOGEN_SUBDIR_MODE"; then
-        $srcdir/configure --enable-gtk-doc --enable-maintainer-mode "$@"
-        echo 
-        echo "Now type 'make' to compile $PROJECT."
+if $srcdir/configure --enable-maintainer-mode --enable-gtk-doc "$@"; then
+  echo
+  echo "Now type 'make' to compile $PROJECT."
+else
+  echo
+  echo "Configure failed or did not finish!"
+  exit 1
 fi
+
