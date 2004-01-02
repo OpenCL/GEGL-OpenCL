0962b43a30408bc736515244a2530977d4df5336
23:38 Daniel Rogers <daniel@phasevelocity.org>
2004-01-02 07:43:40 +0000

    gegl/image/gegl-buffer-double.c gegl/image/gegl-component-sample-model.c
    2004-1-1 23:38 Daniel Rogers <daniel@phasevelocity.org>
    
    	* gegl/image/gegl-buffer-double.c
    	* gegl/image/gegl-component-sample-model.c
    	* tests/gegl-image-buffer-double-test.c
    	* tests/gegl-image-component-sample-model-test.c
    
    	Fixes for stupid mistakes to the GPL notice.

--- tests/gegl-image-component-sample-model-test.c
+++ tests/gegl-image-component-sample-model-test.c
@@ -6,13 +6,13 @@
  *    the Free Software Foundation; either version 2 of the License, or
  *    (at your option) any later version.
  *
- *    Foobar is distributed in the hope that it will be useful,
+ *    GEGL is distributed in the hope that it will be useful,
  *    but WITHOUT ANY WARRANTY; without even the implied warranty of
  *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *    GNU General Public License for more details.
  *
  *    You should have received a copy of the GNU General Public License
- *    along with Foobar; if not, write to the Free Software
+ *    along with GEGL; if not, write to the Free Software
  *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
  *  Copyright 2003 Daniel S. Rogers
