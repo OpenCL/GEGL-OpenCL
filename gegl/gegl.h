d595d877d62923af16d2a378f8c30e40a23238fc
Sven Neumann <sven@gimp.org>
2005-06-01 23:28:11 +0000

    added copyright header.
    2005-06-02  Sven Neumann  <sven@gimp.org>
    
    	* gegl/gegl.h: added copyright header.

--- gegl/gegl.h
+++ gegl/gegl.h
@@ -1,5 +1,27 @@
+/*
+ *   This file is part of GEGL.
+ *
+ *    GEGL is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    GEGL is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with GEGL; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ *  Copyright 2003 Calvin Williamson
+ *
+ */
+
 #ifndef __GEGL_H__
 #define __GEGL_H__
+
 #include <gegl-connection.h>
 #include <gegl-filter.h>
 #include <gegl-graph.h>
@@ -9,4 +31,5 @@
 #include <gegl-property.h>
 #include <gegl-visitable.h>
 #include <gegl-visitor.h>
-#endif
+
+#endif  /* __GEGL_H__ */
