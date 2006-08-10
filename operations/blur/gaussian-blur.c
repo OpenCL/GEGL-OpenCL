4f21ae85b3a2761cd9d54468de92e81ff4ebc1b2
Øyvind Kolås <ok@src.gnome.org>
2006-08-10 07:33:18 +0000

    added copyright statements to operations

--- operations/blur/gaussian-blur.c
+++ operations/blur/gaussian-blur.c
@@ -1,7 +1,22 @@
-/*
- * (c) 2006 Dominik Ernst <dernst@gmx.de>
+/* This file is an image processing operation for GEGL
+ *
+ * GEGL is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * GEGL is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GEGL; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ *
+ * Copyright 2006 Dominik Ernst <dernst@gmx.de>
  */
-
 #ifdef CHANT_SELF
  
 chant_float (radius, 0.0, 200.0, 4.0, "radius of square pixel region.")
--- operations/display/display.c
+++ operations/display/display.c
@@ -1,3 +1,22 @@
+/* This file is an image processing operation for GEGL
+ *
+ * GEGL is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * GEGL is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GEGL; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ *
+ * Copyright 2006 Øyvind Kolås <pippin@gimp.org>
+ */
 #ifdef CHANT_SELF
 
 chant_string(window_title, "window_title", "Title to be given output window")
