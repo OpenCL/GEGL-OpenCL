7ba54eae1cb685ea581f924a3dc19e0512d788d0
Øyvind Kolås <ok@src.gnome.org>
2006-11-02 13:53:08 +0000

    Added some experimental code to play with bayer demosaicing from raw 	files, one loader that only grabs the raw CCD/CMOS data for raw files 	with extension either .rawbayer or .rawbayerS (the S version needed 	for some endianness/pnm reader offset issues).
    	demosaic-simple does a nearest correctly colored neighbour
    	demosaicing. The exact bayer pattern can be specified with integers
    	ranged 0..3.
    
    	* operations/workshop/demosaic-simple.c: NEW file
    	* operations/workshop/rawbayer-load.c: NEW file

---
+++ operations/workshop/demosaic-simple.c
@@ -0,0 +1,238 @@
+/* This file is an image processing operation for GEGL
+ *
+ * GEGL is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * GEGL is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GEGL; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ *
+ * Copyright 2006 Øyvind Kolås <pippin@gimp.org>
+ */
+#if GEGL_CHANT_PROPERTIES
+
+gegl_chant_int (pattern, 0, 3, 0, "Bayer pattern used, 0 seems to work for some nikon files, 2 for some Fuji files.")
+ 
+#else
+
+#define GEGL_CHANT_FILTER
+#define GEGL_CHANT_NAME            demosaic_simple
+#define GEGL_CHANT_DESCRIPTION     "Performs a naive grayscale2color demosaicing of an image, no interpolation."
+#define GEGL_CHANT_SELF            "demosaic-simple.c"
+#define GEGL_CHANT_CATEGORIES      "blur"
+#define GEGL_CHANT_CLASS_INIT
+#include "gegl-chant.h"
+
+static GeglRect get_source_rect (GeglOperation *self);
+
+#include <stdio.h>
+static void
+demosaic (GeglChantOperation *op,
+          GeglBuffer *src,
+          GeglBuffer *dst);
+
+static gboolean
+process (GeglOperation *operation)
+{
+  GeglOperationFilter *filter;
+  GeglChantOperation *self;
+
+  filter = GEGL_OPERATION_FILTER (operation);
+  self   = GEGL_CHANT_OPERATION (operation);
+
+  GeglBuffer *input  = filter->input;
+  GeglBuffer *output;
+
+    {
+      GeglRect   *result = gegl_operation_result_rect (operation);
+      GeglRect    need   = get_source_rect (operation);
+      GeglBuffer *temp_in;
+
+      if (result->w==0 ||
+          result->h==0)
+        {
+          output = g_object_ref (input);
+        }
+      else
+        {
+          temp_in = g_object_new (GEGL_TYPE_BUFFER,
+                                 "source", input,
+                                 "x",      need.x,
+                                 "y",      need.y,
+                                 "width",  need.w,
+                                 "height", need.h,
+                                 NULL);
+
+          output = g_object_new (GEGL_TYPE_BUFFER,
+                                 "format", babl_format ("RGB float"),
+                                 "x",      need.x,
+                                 "y",      need.y,
+                                 "width",  need.w,
+                                 "height", need.h,
+                                 NULL);
+
+          demosaic (self, temp_in, output);
+          g_object_unref (temp_in);
+        }
+
+      {
+        GeglBuffer *cropped = g_object_new (GEGL_TYPE_BUFFER,
+                                              "source", output,
+                                              "x",      result->x,
+                                              "y",      result->y,
+                                              "width",  result->w,
+                                              "height", result->h,
+                                              NULL);
+        filter->output = cropped;
+        g_object_unref (output);
+      }
+    }
+  return  TRUE;
+}
+
+static void
+demosaic (GeglChantOperation *op,
+          GeglBuffer *src,
+          GeglBuffer *dst)
+{
+  gint x,y;
+  gint offset;
+  gfloat *src_buf;
+  gfloat *dst_buf;
+
+  src_buf = g_malloc0 (src->width * src->height * 4);
+  dst_buf = g_malloc0 (dst->width * dst->height * 4 * 3);
+  
+  gegl_buffer_get_fmt (src, src_buf, babl_format ("Y float"));
+
+  offset=0;
+  for (y=src->y; y<dst->height + src->y; y++)
+    for (x=src->x; x<dst->width + src->x; x++)
+      {
+        gfloat red=0.0;
+        gfloat green=0.0;
+        gfloat blue=0.0;
+
+        if (y<dst->height+src->y-1 &&
+            x<dst->width+src->x-1)
+          {
+        if ((y + op->pattern%2)%2==0)
+          {
+            if ((x+op->pattern/2)%2==1)
+              {
+                blue=src_buf[offset+1];
+                green=src_buf[offset];
+                red=src_buf[offset + src->width];
+              }
+            else
+              {
+                blue=src_buf[offset];
+                green=src_buf[offset+1];
+                red=src_buf[offset+1+src->width];
+              }
+          }
+        else
+          {
+            if ((x+op->pattern/2)%2==1)
+              {
+                blue=src_buf[offset + src->width + 1];
+                green=src_buf[offset + 1];
+                red=src_buf[offset];
+              }
+            else
+              {
+                blue=src_buf[offset + src->width];
+                green=src_buf[offset];
+                red=src_buf[offset + 1];
+              }
+          }
+        }
+        
+        dst_buf [offset*3+0] = red;
+        dst_buf [offset*3+1] = green;
+        dst_buf [offset*3+2] = blue;
+
+        offset++;
+      }
+
+  gegl_buffer_set_fmt (dst, dst_buf, babl_format ("RGB float"));
+  g_free (src_buf);
+  g_free (dst_buf);
+}
+
+#include <math.h>
+static GeglRect 
+get_defined_region (GeglOperation *operation)
+{
+  GeglRect  result = {0,0,0,0};
+  GeglRect *in_rect = gegl_operation_source_get_defined_region (operation,
+                                                                "input");
+  if (!in_rect)
+    return result;
+
+  result = *in_rect;
+  if (result.w != 0 &&
+      result.h != 0)
+    {
+      result.w+=1;
+      result.h+=1;
+    }
+  
+  return result;
+}
+
+static GeglRect get_source_rect (GeglOperation *self)
+{
+  GeglRect            rect;
+
+  rect  = *gegl_operation_get_requested_region (self);
+  if (rect.w != 0 &&
+      rect.h != 0)
+    {
+      rect.w += 1;
+      rect.h += 1;
+    }
+
+  return rect;
+}
+
+static gboolean
+calc_source_regions (GeglOperation *self)
+{
+  GeglRect need = get_source_rect (self);
+
+  gegl_operation_set_source_region (self, "input", &need);
+
+  return TRUE;
+}
+
+static GeglRect
+get_affected_region (GeglOperation *self,
+                     const gchar   *input_pad,
+                     GeglRect       region)
+{
+  gint                radius;
+ 
+  radius = ceil(1);
+
+  region.w += 1;
+  region.h += 1;
+  return region;
+}
+
+static void class_init (GeglOperationClass *operation_class)
+{
+  operation_class->get_defined_region  = get_defined_region;
+  operation_class->get_affected_region = get_affected_region;
+  operation_class->calc_source_regions = calc_source_regions;
+}
+
+#endif
---
+++ operations/workshop/rawbayer-load.c
@@ -0,0 +1,135 @@
+/* This file is an image processing operation for GEGL
+ *
+ * GEGL is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * GEGL is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GEGL; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ *
+ * Copyright 2006 Øyvind Kolås <pippin@gimp.org>
+ */
+#if GEGL_CHANT_PROPERTIES
+gegl_chant_string (path, "/tmp/test.raw",
+                   "Path of file to load.")
+#else
+
+#define GEGL_CHANT_SOURCE
+#define GEGL_CHANT_NAME            rawbayer_load
+#define GEGL_CHANT_DESCRIPTION     "Raw image loader, wrapping dcraw with pipes, provides the raw bayer grid as grayscale, if the fileformat is .rawbayer it will use this loader instead of the normal dcraw loader, if the fileformat is .rawbayerS it will swap the returned 16bit numbers (the pnm loader is apparently buggy)"
+
+#define GEGL_CHANT_SELF            "rawbayer-load.c"
+#define GEGL_CHANT_CATEGORIES      "hidden"
+#define GEGL_CHANT_CLASS_INIT
+#include "gegl-chant.h"
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define PIPE_MODE "r"
+#define MAX_SAMPLE 65535
+#define ERROR -1
+
+static void load_buffer (GeglChantOperation *op_raw_load);
+
+static gboolean
+process (GeglOperation *operation)
+{
+  GeglOperationSource *op_source = GEGL_OPERATION_SOURCE(operation);
+  GeglChantOperation       *self      = GEGL_CHANT_OPERATION (operation);
+
+  g_assert (self->priv);
+  op_source->output = GEGL_BUFFER (self->priv);
+  self->priv = NULL;
+  return TRUE;
+}
+
+
+static GeglRect
+get_defined_region (GeglOperation *operation)
+{
+  GeglRect result = {0,0,0,0};
+  GeglChantOperation *self = GEGL_CHANT_OPERATION (operation);
+
+  load_buffer (self);
+
+  result.w = GEGL_BUFFER (self->priv)->width;
+  result.h = GEGL_BUFFER (self->priv)->height;
+  return result;
+}
+
+static void class_init (GeglOperationClass *klass)
+{
+  gegl_extension_handler_register (".rawbayer", "rawbayer-load");
+  gegl_extension_handler_register (".rawbayerS", "rawbayer-load");
+}
+
+static void
+load_buffer (GeglChantOperation *op_raw_load)
+{
+  if (!op_raw_load->priv)
+    {
+      FILE *pfp;
+      gchar *command;
+
+      gint width, height, val_max;
+      char newline;
+
+      command = g_malloc (strlen (op_raw_load->path) + 128);
+      if (!command)
+        return;
+      sprintf (command, "dcraw -j -d -4 -c '%s'\n", op_raw_load->path);
+      pfp = popen (command, PIPE_MODE);
+      free (command);
+
+      if (fscanf (pfp, "P6 %d %d %d %c",
+         &width, &height, &val_max, &newline) != 4)
+        {
+          pclose (pfp);
+          g_warning ("not able to aquire raw data");
+          return;
+        }
+
+       op_raw_load->priv = g_object_new (GEGL_TYPE_BUFFER,
+                                        "format", babl_format ("Y u16"),
+                                        "x",      0,
+                                        "y",      0,
+                                        "width",  width,
+                                        "height", height,
+                                        NULL);
+         {
+           guchar *buf=g_malloc (width * height * 3 * 2);
+           fread (buf, 1, width * height * 3 * 2, pfp);
+           if(strstr (op_raw_load->path, "rawbayerS")){
+             gint i;
+             for (i=0;i<width*height*3;i++)
+               {
+                guchar tmp = buf[i*2];
+                buf[i*2] = buf[i*2+1];
+                buf[i*2+1] = tmp;
+               }
+           }
+           gegl_buffer_set_fmt (GEGL_BUFFER (op_raw_load->priv), buf,
+                                        babl_format_new (
+                                          babl_model ("RGB"),
+                                          babl_type ("u16"),
+                                          babl_component ("R"),
+                                          babl_component ("G"),
+                                          babl_component ("B"),
+                                          NULL));
+           g_free (buf);
+         }
+       fclose (pfp);
+    }
+}
+
+#endif
