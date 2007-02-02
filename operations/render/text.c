d6f064af4252d8d1cb3628fc68d565c541c68f97
Øyvind Kolås <ok@src.gnome.org>
2007-02-02 15:58:57 +0000

    only process for the result rect not the requested rect. (massive speedup
    * operations/render/text.c: (process): only process for the result
    rect not the requested rect. (massive speedup in some circumstances).
    Getting babl to use the optimized conversion for cairo originated data
    is the next obvious optimization.
    
    svn path=/trunk/; revision=1298

--- operations/render/text.c
+++ operations/render/text.c
@@ -142,29 +142,29 @@ process (GeglOperation *operation,
 {
   GeglChantOperation *self = GEGL_CHANT_OPERATION (operation);
   GeglBuffer         *output = NULL;
-  GeglRectangle      *need;
+  GeglRectangle      *result;
 
-  need = gegl_operation_get_requested_region (operation, context_id);
+  result = gegl_operation_result_rect (operation, context_id);
 
   output = g_object_new (GEGL_TYPE_BUFFER,
                          "format", babl_format ("R'G'B'A u8"), /* FIXME: babl
                                                          performs a wrong conversion if the
                                                          correct format is specified here. */
-                         "x",      need->x,
-                         "y",      need->y,
-                         "width",  need->w,
-                         "height", need->h,
+                         "x",      result->x,
+                         "y",      result->y,
+                         "width",  result->w,
+                         "height", result->h,
                          NULL);
 
   {
-    guchar *data = g_malloc0 (need->w * need->h * 4);
+    guchar *data = g_malloc0 (result->w * result->h * 4);
     cairo_t *cr;
 
     cairo_surface_t *surface = cairo_image_surface_create_for_data (data, CAIRO_FORMAT_ARGB32,
-      need->w, need->h, need->w* 4);
+      result->w, result->h, result->w* 4);
     cr = cairo_create (surface);
     cairo_set_source_rgba (cr, 1.0, 1.0, 1.0, 1.0);
-    cairo_translate (cr, -need->x, -need->y);
+    cairo_translate (cr, -result->x, -result->y);
     text_layout_text (self, cr, 0, NULL, NULL);
 
     gegl_buffer_set (output,
