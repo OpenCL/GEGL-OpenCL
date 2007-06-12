72f745809ab3d4694c494bfd8a15ff0362f5309b
Mukund Sivaraman <muks@mukund.org>
2007-06-12 06:45:11 +0000

    added <errno.h> include without which build breaks on Fedora 7.
    2007-06-12  Mukund Sivaraman  <muks@mukund.org>
    
            * operations/io/ff-load.c: added <errno.h> include without which
            build breaks on Fedora 7.
    
    
    svn path=/trunk/; revision=1558

--- operations/io/ff-load.c
+++ operations/io/ff-load.c
@@ -33,7 +33,9 @@ gegl_chant_int (frame, 0, 1000000, 0, "frame number")
 #define GEGL_CHANT_INIT
 #define GEGL_CHANT_CLASS_INIT
 #include "gegl-chant.h"
-#include "ffmpeg/avformat.h"
+
+#include <errno.h>
+#include <ffmpeg/avformat.h>
 
 typedef struct
 {
