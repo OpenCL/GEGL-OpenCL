66375778d980c9f15a01565fe4d554300fa1f85b
Étienne Bersac <bersace@src.gnome.org>
2007-09-24 23:44:50 +0000

    Added convert-format op.
    svn path=/trunk/; revision=1663

---
+++ operations/core/convert-format.c
@@ -0,0 +1,58 @@
+/* This file is an image processing operation for GEGL
+ *
+ * GEGL is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * GEGL is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GEGL; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
+ *
+ * Copyright 2007 Étienne Bersac <bersace03@laposte.net>
+ * Copyright 2006 Øyvind Kolås <pippin@gimp.org>
+ *
+ * This operation is just a forked grey op with format parameters.
+ */
+#if GEGL_CHANT_PROPERTIES
+gegl_chant_string(format, "RGBA float", "Babl ouput format string")
+#else
+
+#define GEGL_CHANT_NAME          convert_format
+#define GEGL_CHANT_SELF          "convert-format.c"
+#define GEGL_CHANT_DESCRIPTION   "Convert the data into the specified format"
+#define GEGL_CHANT_CATEGORIES    "core:color"
+
+#define GEGL_CHANT_POINT_FILTER
+#define GEGL_CHANT_PREPARE
+
+#include "gegl-chant.h"
+
+static void prepare (GeglOperation *operation,
+                     gpointer       context_id)
+{
+  GeglChantOperation  *self = GEGL_CHANT_OPERATION (operation);
+  g_assert (self->format);
+  Babl *format = babl_format (self->format);
+  /* check format ? */
+
+  gegl_operation_set_format (operation, "input", format);
+  gegl_operation_set_format (operation, "output", format);
+}
+
+static gboolean
+process (GeglOperation *op,
+         void          *in_buf,
+         void          *out_buf,
+         glong          samples) 
+{
+  return TRUE;
+}
+
+#endif
