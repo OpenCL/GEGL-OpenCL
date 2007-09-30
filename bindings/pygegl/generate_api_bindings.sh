7df563c4849d033fef5c3ea28efaa9937d3c6b50
Kevin Cozens <kcozens@cvs.gnome.org>
2007-09-30 13:47:48 +0000

    Added check for existence of automake-1.10.
    2007-09-30  Kevin Cozens  <kcozens@cvs.gnome.org>
    
    	* autogen.sh: Added check for existence of automake-1.10.
    
    	* gegl.defs
    	* gegl.override: Updated due to API change of gegl_node_blit().
    
    	* generate_api_bindings.sh: Updated path to header file.
    
    svn path=/trunk/; revision=1670

--- bindings/pygegl/generate_api_bindings.sh
+++ bindings/pygegl/generate_api_bindings.sh
@@ -1,9 +1,13 @@
+if test "x$PREFIX" = "x"; then
+    PREFIX=/usr/local
+fi
+
 CODE_GEN=/usr/share/pygtk/2.0/codegen/h2def.py
-INCLUDE=/usr/local/include/gegl-1.0
+INCLUDE=$PREFIX/include/gegl-1.0
 
 NEWDEFS=gegl.defs_`date --rfc-3339=date`
 
 #python $CODE_GEN `grep include $INCLUDE/gegl.h | grep gegl | sed -e "s@.*<\(.*\)>@$INCLUDE/\1@"` > out.defs
-python $CODE_GEN $INCLUDE/gegl.h $INCLUDE/gegl-utils.h > out.defs
+python $CODE_GEN $INCLUDE/gegl.h $INCLUDE/gegl/gegl-utils.h > out.defs
 
 gvim -d out.defs gegl.defs
