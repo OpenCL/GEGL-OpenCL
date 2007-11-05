49511febbab319396c37002f51a8fb21e906641c
Øyvind Kolås <ok@src.gnome.org>
2007-11-05 22:45:52 +0000

    Emit "computed" signal for cache (and thus in turn node) if a cache is
    * gegl/process/gegl-eval-visitor.c: (visit_pad): Emit "computed"
    signal for cache (and thus in turn node) if a cache is associated
    with the node being processed.
    
    svn path=/trunk/; revision=1676

--- gegl/process/gegl-eval-visitor.c
+++ gegl/process/gegl-eval-visitor.c
@@ -91,6 +91,9 @@ visit_pad (GeglVisitor *self,
               /* update the valid region of the cache */
               gegl_region_union_with_rect (node->cache->valid_region,
                                            gegl_operation_result_rect (operation, context_id));
+
+			  /* XXX: this should perhaps be the requested rect and not the result rect? */
+              g_signal_emit (node->cache, gegl_cache_signals[GEGL_CACHE_COMPUTED], 0, gegl_operation_result_rect (operation, context_id), NULL);
             }
         }
     }
