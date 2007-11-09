847234901b1922186d768daeaa6e03e5f88609d9
Øyvind Kolås <ok@src.gnome.org>
2007-11-09 22:01:58 +0000

    adjusted license from GPLv2 or later to LGPLv2 or later, it being GPL was
    * gegl/gegl-dot.[ch]: adjusted license from GPLv2 or later to LGPLv2
    or later, it being GPL was a copy and paste mistake.
    
    svn path=/trunk/; revision=1678

--- gegl/gegl-dot.h
+++ gegl/gegl-dot.h
@@ -1,20 +1,21 @@
-/* This file is part of GEGL editor -- a gtk frontend for GEGL
+/* This file is the public GEGL API
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * GEGL is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * GEGL is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GEGL; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301, USA.
  *
- * Copyright (C) 2006 Øyvind Kolås
+ * 2006 © Øyvind Kolås.
  */
 
 #ifndef GEGL_DOT_H
