da77725c121c0158c07a9d41f819b8074183f26e
Manish Singh <yosh@gimp.org>
2007-11-25 01:44:07 +0000

    Gegl/Makefile.am place for higher level python wrapping.
    2007-11-24  Manish Singh  <yosh@gimp.org>
    
            * Gegl/Makefile.am
            * Gegl/fifthleg.py: place for higher level python wrapping.
    
            * Gegl/__init__.py: import fifthleg.
    
            * Gegl/gegl.defs: updates for new xml parsing/serialization API.
    
            * Gegl/gegl.override: implementation for gegl_node_new_from_xml
            binding.
    
    svn path=/trunk/; revision=1724

--- bindings/pygegl/Gegl/fifthleg.py
+++ bindings/pygegl/Gegl/fifthleg.py
@@ -1,7 +1,6 @@
-# PyGEGL - Python bindings for the GEGL image processing library
 # Copyright (C) 2007 Manish Singh
 #
-#   __init__.py: initialization file for the Gegl package
+#   fifthleg.py: higher level API for the Gegl package
 #
 # PyGEGL is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -16,15 +15,21 @@
 # You should have received a copy of the GNU Lesser General Public
 # License along with PyGEGL; if not, see <http://www.gnu.org/licenses/>.
 
-import dl
-libgegl = dl.open("libgegl-1.0.so.0", dl.RTLD_NOW|dl.RTLD_GLOBAL)
-libbabl = dl.open("libbabl-0.0.so.0", dl.RTLD_NOW|dl.RTLD_GLOBAL)
+import _gegl
 
-from _gegl import *
-from fifthleg import *
+def node_new_from_file(file=None):
+    path_root = None
 
-import atexit
-atexit.register(exit)
-del exit, atexit
+    if file is None:
+        import sys
+        file = sys.stdin
 
-del _gegl
+    if isinstance(file, basestring):
+        filename = file
+        file = open(filename)
+
+        import os
+        path_root = os.path.realpath(os.path.dirname(filename))
+        
+    data = file.read()
+    return _gegl.parse_xml(data)
