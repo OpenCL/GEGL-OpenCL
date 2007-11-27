2aae9f41daccddc9934b74d126cc431d257f835c
Martin Nordholts <martinn@svn.gnome.org>
2007-11-27 21:31:43 +0000

    Cosmetics on the function propotyopes merged from gegl-graph.h.
    2007-11-27  Martin Nordholts  <martinn@svn.gnome.org>
    
    	* gegl/graph/gegl-node.h: Cosmetics on the function propotyopes
    	merged from gegl-graph.h.
    
    svn path=/trunk/; revision=1737

--- gegl/graph/gegl-node.h
+++ gegl/graph/gegl-node.h
@@ -168,27 +168,27 @@ void          gegl_node_set_need_rect       (GeglNode      *node,
 
 /* Graph related member functions of the GeglNode class */
 
-GeglNode *    gegl_node_add_child           (GeglNode    *self,
-                                             GeglNode    *child);
-GeglNode *    gegl_node_remove_child        (GeglNode    *self,
-                                             GeglNode     *child);
-GeglNode *    gegl_node_get_nth_child       (GeglNode    *self,
-                                             gint          n);
-GSList   *    gegl_node_get_children        (GeglNode    *self);
-void          gegl_node_remove_children     (GeglNode    *self);
-gint          gegl_node_get_num_children    (GeglNode    *self);
+GeglNode *    gegl_node_add_child           (GeglNode      *self,
+                                             GeglNode      *child);
+GeglNode *    gegl_node_remove_child        (GeglNode      *self,
+                                             GeglNode      *child);
+GeglNode *    gegl_node_get_nth_child       (GeglNode      *self,
+                                             gint           n);
+GSList   *    gegl_node_get_children        (GeglNode      *self);
+void          gegl_node_remove_children     (GeglNode      *self);
+gint          gegl_node_get_num_children    (GeglNode      *self);
 
 GeglNode *    gegl_node_new                 (void);
 
-GeglNode *    gegl_node_new_child           (GeglNode    *self,
-                                             const gchar  *first_property_name,
+GeglNode *    gegl_node_new_child           (GeglNode      *self,
+                                             const gchar   *first_property_name,
                                              ...) G_GNUC_NULL_TERMINATED;
-GeglNode *    gegl_node_create_child        (GeglNode    *self,
-                                             const gchar  *operation);
-GeglNode *    gegl_node_get_input_proxy     (GeglNode     *graph,
-                                             const gchar  *name);
-GeglNode *    gegl_node_get_output_proxy    (GeglNode     *graph,
-                                             const gchar  *name);
+GeglNode *    gegl_node_create_child        (GeglNode      *self,
+                                             const gchar   *operation);
+GeglNode *    gegl_node_get_input_proxy     (GeglNode      *graph,
+                                             const gchar   *name);
+GeglNode *    gegl_node_get_output_proxy    (GeglNode      *graph,
+                                             const gchar   *name);
 
 const gchar * gegl_node_get_operation       (GeglNode      *node);
 
