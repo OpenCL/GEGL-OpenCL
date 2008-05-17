9cd77ccb9c794877609d7dbccea9a31e020c23fb
Øyvind Kolås <pippin@gimp.org>
2008-05-17 22:26:01 +0000

    renamed to:
    2008-05-17  Øyvind Kolås  <pippin@gimp.org>
    
    	* examples/2gegl.c: renamed to:
    	* examples/2geglbuffer.c:
    	* examples/clock.c: renamed to:
    	* examples/geglbuffer-clock.c:
    	* examples/geglbuffer-add-image.c: added.
    	* examples/multiplayer-paint.sh: make use of new files.
    	* examples/Makefile.am: added subdir data
    	* examples/data: added
    	* examples/data/Makefile.am: added
    	* examples/data/surfer.png: added
    	* configure.ac: added examples/data/Makefile
    
    svn path=/trunk/; revision=2315

---
+++ examples/geglbuffer-add-image.c
@@ -0,0 +1,64 @@
+#include <gegl.h>
+#include <glib/gprintf.h>
+
+gint
+main (gint    argc,
+      gchar **argv)
+{
+  GeglBuffer *buffer; /* instantiate a graph */
+  GeglNode   *gegl;   /* the gegl graph we're using as a node factor */
+  GeglNode   *write_buffer,
+             *shift,
+             *load;
+  gchar *in_file;
+  gchar *buf_file;
+  gdouble x;
+  gdouble y;
+
+  gegl_init (&argc, &argv);  /* initialize the GEGL library */
+
+
+  if (argv[1]==NULL ||
+      argv[2]==NULL ||
+      argv[3]==NULL ||
+      argv[4]==NULL)
+    {
+      g_print ("\nUsage: %s <gegl buffer> <image file> <x> <y>\n"
+               "\nWrites an image into the GeglBuffer at the specified coordinates\n",
+               argv[0]);
+      return -1;
+    }
+  buf_file = argv[1];
+  in_file = argv[2];
+  x = atof (argv[3]);
+  y = atof (argv[4]);
+
+  buffer = gegl_buffer_open (buf_file);
+  gegl = gegl_node_new ();
+
+  write_buffer = gegl_node_new_child (gegl,
+                                    "operation", "write-buffer",
+                                    "buffer", buffer,
+                                    NULL);
+  shift      = gegl_node_new_child (gegl,
+                                    "operation", "shift",
+                                    "x", x,
+                                    "y", y,
+                                    NULL);
+  load        = gegl_node_new_child (gegl,
+                                   "operation", "load",
+                                   "path", in_file,
+                                   NULL);
+
+  gegl_node_link_many (load, shift, write_buffer, NULL);
+  gegl_node_process (write_buffer);
+
+  /* free resources used by the graph and the nodes it owns */
+  g_object_unref (gegl);
+  g_object_unref (buffer);
+
+  /* free resources globally used by GEGL */
+  gegl_exit ();
+
+  return 0;
+}
--- examples/multiplayer-paint.sh
+++ examples/multiplayer-paint.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-./2gegl data/car-stack.jpg test.gegl
-./clock test.gegl &
+./2geglbuffer data/surfer.png test.gegl
+./geglbuffer-add-image test.gegl data/surfer.png 64 64
+./geglbuffer-clock test.gegl &
+
 ./gegl-paint test.gegl &
 ./gegl-paint test.gegl &
+
 gegl -x '<gegl><open-buffer path="test.gegl"/></gegl>'
 
-killall -9 clock gegl-paint lt-clock lt-gegl-paint
-
-
+killall -9 geglbuffer-clock gegl-paint lt-geglbuffer-clock lt-gegl-paint
