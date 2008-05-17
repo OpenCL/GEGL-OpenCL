2d441dd0326122dd1416a55f1b98099bcffbc16e
Øyvind Kolås <ok@src.gnome.org>
2008-05-17 17:04:32 +0000

    improved launcher script with a monitor using the gegl binary that has
    * examples/multiplayer-paint.sh: improved launcher script with a
    monitor using the gegl binary that has live update of clock and the
    two concurrent painters.
    
    svn path=/trunk/; revision=2304

--- examples/multiplayer-paint.sh
+++ examples/multiplayer-paint.sh
@@ -3,8 +3,9 @@
 ./2gegl data/car-stack.jpg test.gegl
 ./clock test.gegl &
 ./gegl-paint test.gegl &
-./gegl-paint test.gegl
+./gegl-paint test.gegl &
+gegl -x '<gegl><open-buffer path="test.gegl"/></gegl>'
 
-killall -9 clock gegl-paint
+killall -9 clock gegl-paint lt-clock lt-gegl-paint
 
 
