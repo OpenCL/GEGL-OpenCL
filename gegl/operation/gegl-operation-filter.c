9fa14eb0f43c250aff1464e69c2a4139f05fd1ed
Øyvind Kolås <ok@src.gnome.org>
2008-11-24 01:19:13 +0000

    allow NULL inputs.
    * gegl/operation/gegl-operation-filter.c:
    (gegl_operation_filter_process): allow NULL inputs.
    
    svn path=/trunk/; revision=2801

--- gegl/operation/gegl-operation-filter.c
+++ gegl/operation/gegl-operation-filter.c
@@ -204,7 +204,7 @@ gegl_operation_filter_process (GeglOperation   *operation,
 
   input  = gegl_operation_context_get_source (context, "input");
   output = gegl_operation_context_get_target (context, "output");
-  if (input != NULL)
+  if (input != NULL || 1)
     {
       success = klass->process (operation, input, output, result);
       g_object_unref (input);
