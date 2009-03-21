8633682d743056b5394fcf0a84df6b12d6d26e1b
Martin Nordholts <martinn@src.gnome.org>
2009-03-21 11:26:11 +0000

    Port buffer tests to automake
    Port buffer tests to automake which has a couple of benefits. First of
    all they are run and results reported during a normal make check, and
    second of all the tests are run against the build and not against a
    possibly non-existant installation based on hardcoded prefixes.
    
    svn path=/trunk/; revision=2976

---
+++ tests/buffer/buffer-tests-results.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+total_tests=`ls -1 reference | wc -l`
+successful_tests=`cat buffer-tests-report | grep identical | wc -l`
+
+echo $successful_tests of $total_tests tests succesful.
+
+if [ $total_tests -eq $successful_tests ]
+then
+exit 0;
+else
+exit -1;
+fi
