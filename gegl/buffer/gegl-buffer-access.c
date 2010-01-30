166c6d3800dcab9eb27f4472c3942d2adf5d5a6e
Øyvind Kolås <pippin@gimp.org>
2010-01-30 14:10:53 +0000

    gegl_buffer_sample: recreate the sampler if format changes
    If samplings were done on the same buffer using different formats,
    the first format would always be expected, potentially overrunning
    memory.

--- gegl/buffer/gegl-buffer-access.c
+++ gegl/buffer/gegl-buffer-access.c
@@ -1095,10 +1095,13 @@ gegl_buffer_sample (GeglBuffer       *buffer,
 
   desired_type = gegl_sampler_type_from_interpolation (interpolation);
 
+  /* unset the cached sampler if it dosn't match the needs */
   if (buffer->sampler != NULL &&
-      !G_TYPE_CHECK_INSTANCE_TYPE (buffer->sampler, desired_type))
+     (!G_TYPE_CHECK_INSTANCE_TYPE (buffer->sampler, desired_type) || 
+       buffer->sampler_format != format
+      ))
     {
-      g_object_unref(buffer->sampler);
+      g_object_unref (buffer->sampler);
       buffer->sampler = NULL;
     }
 
