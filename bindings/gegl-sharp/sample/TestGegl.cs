c7f63178697cfbacd367b97473eebe826898a7b2
Ruben Vermeersch <ruben@savanne.be>
2010-08-11 13:43:38 +0200

    Update gegl-sharp bindings for libgegl-0.1.so.

--- bindings/gegl-sharp/sample/TestGegl.cs
+++ bindings/gegl-sharp/sample/TestGegl.cs
@@ -8,21 +8,21 @@ public class Test {
 
         Node graph = new Node();
 
-        Node load       = graph.CreateChild("load");
+        Node load       = graph.CreateChild("gegl:load");
         load.SetProperty("path", "../../../docs/gallery/data/car-stack.jpg");
 
-        Node scale      = graph.CreateChild("scale");
+        Node scale      = graph.CreateChild("gegl:scale");
         scale.SetProperty("x", 0.50);
         scale.SetProperty("y", 0.50);
 
-        Node rotate      = graph.CreateChild("rotate");
+        Node rotate      = graph.CreateChild("gegl:rotate");
         rotate.SetProperty("degrees", 45.0);
 
-        Node brightness_contrast = graph.CreateChild("brightness-contrast");
+        Node brightness_contrast = graph.CreateChild("gegl:brightness-contrast");
         brightness_contrast.SetProperty("contrast", 4.0);
         brightness_contrast.SetProperty("brightness", -2.0);
 
-        Node save       = graph.CreateChild("png-save");
+        Node save       = graph.CreateChild("gegl:png-save");
         save.SetProperty("path", "sample-out.png");
 
         load.Link(scale);
