700379f6733c9279592498b4a071c00da99715b2
Michael Schumacher <schumaml@gmx.de>
2010-11-24 23:34:56 +0100

    Use #!/usr/bin/env ruby instead of #!/usr/bin/ruby

--- tools/gobj2dot.rb
+++ tools/gobj2dot.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/ruby
+#!/usr/bin/env ruby
 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
