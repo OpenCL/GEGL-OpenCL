26a18b34bcbc9530c719e8d3e48eafb06fe8c640
Mukund Sivaraman <muks@banu.com>
2011-08-30 19:54:10 +0530

    lens-correct: Move stuff in lens-correct.h into lens-correct.c
    There's no reason this has to be in an external header.

--- operations/workshop/lens-correct.c
+++ operations/workshop/lens-correct.c
@@ -88,9 +88,31 @@ gegl_chant_double (alpha_d, _("Model alpha d:"), 0.0, 2.0, 1.0,
 
 #include "gegl-chant.h"
 #include <math.h>
-#include "lens-correct.h"
-
 #include <stdio.h>
+
+/* Struct containing the correction parameters a,b,c,d for a lens color
+ * channel.  These parameters as the same as used in the Panotools
+ * system.  For a detailed explanation, please consult
+ * http://wiki.panotools.org/Lens_correction_model
+ *
+ * Normally a, b, and c are close to zero, and d is close to one.  Note
+ * that d is the parameter that's approximately equal to 1 - a - b - c,
+ * NOT one of the image shift parameters as used in some GUIs.
+ */
+typedef struct {
+  gfloat a, b, c, d;
+} ChannelCorrectionModel;
+
+/* Struct containing all the information required for lens correction.
+ * It includes the total size of the image plus the correction
+ * parameters for each color channel.
+ */
+typedef struct {
+  GeglRectangle BB;                               /* Bounding box of the imaged area. */
+  gfloat cx, cy;                                  /* Coordinates of lens center within the imaged area.  */
+  gfloat rscale;                                  /* Scale of the image (1/2 of the shortest side). */
+  ChannelCorrectionModel red, green, blue, alpha; /* Correction parameters for each color channel. */
+} LensCorrectionModel;
 
 static void
 make_lens (LensCorrectionModel *lens,
