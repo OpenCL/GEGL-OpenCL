4f5e2499eeefb7f236ee581486d4ec4886f49b70
Øyvind Kolås <pippin@gimp.org>
2012-03-17 17:34:36 +0000

    add missing gegl-apply header

---
+++ gegl/gegl-apply.h
@@ -0,0 +1,32 @@
+/* This file is part of GEGL
+ *
+ * GEGL is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * GEGL is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GEGL; if not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2003 Calvin Williamson
+ *           2006 Øyvind Kolås
+ */
+
+#ifndef __GEGL_APPLY_H__
+#define __GEGL_APPLY_H__
+
+#include "operation/gegl-operation-context.h"
+#include <gegl/buffer/gegl-buffer.h>
+
+G_BEGIN_DECLS
+
+
+
+G_END_DECLS
+
+#endif /* __GEGL_APPLY_H__ */
