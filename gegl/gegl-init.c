19715e2b855bec6175e2e7a91858dca66abc3409
Victor Oliveira <victormatheus@gmail.com>
2012-04-23 12:10:46 -0300

    removing some unnecessary verifications of gegl_config()->use_opencl
    gegl_cl_is_accelerated() already verifies it.

--- gegl/gegl-init.c
+++ gegl/gegl-init.c
@@ -595,10 +595,10 @@ gegl_post_parse_hook (GOptionContext *context,
   swap_clean ();
 
   /* Initialize OpenCL if wanted and possible */
-  if (gegl_config()->use_opencl)
+  if (gegl_cl_is_accelerated ())
     gegl_cl_init (NULL);
 
-  GEGL_NOTE (GEGL_DEBUG_OPENCL, "Using OpenCL: %d\n", gegl_config()->use_opencl && gegl_cl_is_accelerated ());
+  GEGL_NOTE (GEGL_DEBUG_OPENCL, "Using OpenCL: %d\n", gegl_cl_is_accelerated ());
 
   return TRUE;
 }
