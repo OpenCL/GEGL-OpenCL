a8a6dd25ba950c87013b0bdd7903fc1a8f8ced29
Michael Muré <batolettre@gmail.com>
2012-06-08 22:33:46 +0900

    fractal-explorer: enhance in modern GEGL way * produce float colors * produce color for infinite plane * inherit from point-renderer for speed * cleaning

--- operations/common/fractal-explorer.c
+++ operations/common/fractal-explorer.c
@@ -27,11 +27,6 @@
 
 #ifdef GEGL_CHANT_PROPERTIES
 
-gegl_chant_int_ui (width,  _("Width"),  1, 10000000, 400, 1, 2000, 1.5,
-                   _("Width"))
-gegl_chant_int_ui (height, _("Height"), 1, 10000000, 400, 1, 2000, 1.5,
-                   _("Height"))
-
 gegl_chant_register_enum (gegl_fractal_explorer_type)
   enum_value (GEGl_FRACTAL_EXPLORER_TYPE_MANDELBROT,   "Mandelbrot")
   enum_value (GEGl_FRACTAL_EXPLORER_TYPE_JULIA,        "Julia")
@@ -47,11 +42,6 @@ gegl_chant_register_enum_end (GeglFractalExplorerType)
 gegl_chant_enum (fractaltype, _("Fractal type"), GeglFractalExplorerType,
                  gegl_fractal_explorer_type, GEGl_FRACTAL_EXPLORER_TYPE_MANDELBROT,
                  _("Type of a fractal"))
-
-gegl_chant_double (xmin, _("Left"),   -3.0, 3.0, -2.0, _("Left"))
-gegl_chant_double (xmax, _("Right"),  -3.0, 3.0,  2.0, _("Right"))
-gegl_chant_double (ymin, _("Top"),    -3.0, 3.0, -2.0, _("Top"))
-gegl_chant_double (ymax, _("Bottom"), -3.0, 3.0,  2.0, _("Bottom"))
 
 gegl_chant_int (iter, _("Iterations"), 1, 1000, 50, _("Iterations"))
 
@@ -96,7 +86,7 @@ gegl_chant_boolean (useloglog, _("Loglog smoothing"), FALSE,
 
 #else
 
-#define GEGL_CHANT_TYPE_SOURCE
+#define GEGL_CHANT_TYPE_POINT_RENDER
 #define GEGL_CHANT_C_FILE       "fractal-explorer.c"
 
 #include "gegl-chant.h"
@@ -105,216 +95,18 @@ gegl_chant_boolean (useloglog, _("Loglog smoothing"), FALSE,
 
 typedef struct
 {
-  guchar r, g, b;
-} gucharRGB;
+  gfloat r, g, b;
+} gfloatRGB;
 
-typedef gucharRGB  clrmap[MAXNCOLORS];
-
-
-static void
-explorer_render_row (GeglChantO *o,
-                     gint        col_start,
-                     gint        col_end,
-                     gint        row,
-                     clrmap      colormap,
-                     guchar    **dest_row)
-{
-  gint    col;
-  gdouble a;
-  gdouble b;
-  gdouble x;
-  gdouble y;
-  gdouble oldx;
-  gdouble oldy;
-  gdouble tempsqrx;
-  gdouble tempsqry;
-  gdouble tmpx = 0;
-  gdouble tmpy = 0;
-  gdouble foldxinitx;
-  gdouble foldxinity;
-  gdouble foldyinitx;
-  gdouble foldyinity;
-  gdouble xx = 0;
-  gdouble adjust;
-  gint    counter;
-  gint    color;
-  gdouble xdiff;
-  gdouble ydiff;
-  gdouble log2;
-
-  log2 = log (2.0);
-
-  xdiff = (o->xmax - o->xmin) / o->width;
-  ydiff = (o->ymax - o->ymin) / o->height;
-
-  for (col = col_start; col < col_end; col++)
-    {
-      a = o->xmin + (gdouble) col * xdiff;
-      b = o->ymin + (gdouble) row * ydiff;
-      if (o->fractaltype != GEGl_FRACTAL_EXPLORER_TYPE_MANDELBROT)
-        {
-          tmpx = x = a;
-          tmpy = y = b;
-        }
-      else
-        {
-          x = 0;
-          y = 0;
-        }
-
-      for (counter = 0; counter < o->iter; counter++)
-        {
-          oldx=x;
-          oldy=y;
-
-          switch (o->fractaltype)
-            {
-            case GEGl_FRACTAL_EXPLORER_TYPE_MANDELBROT:
-              xx = x * x - y * y + a;
-              y = 2.0 * x * y + b;
-              break;
-
-            case GEGl_FRACTAL_EXPLORER_TYPE_JULIA:
-              xx = x * x - y * y + o->cx;
-              y = 2.0 * x * y + o->cy;
-              break;
-
-            case GEGl_FRACTAL_EXPLORER_TYPE_BARNSLEY_1:
-              foldxinitx = oldx * o->cx;
-              foldyinity = oldy * o->cy;
-              foldxinity = oldx * o->cy;
-              foldyinitx = oldy * o->cx;
-              /* orbit calculation */
-              if (oldx >= 0)
-                {
-                  xx = (foldxinitx - o->cx - foldyinity);
-                  y  = (foldyinitx - o->cy + foldxinity);
-                }
-              else
-                {
-                  xx = (foldxinitx + o->cx - foldyinity);
-                  y  = (foldyinitx + o->cy + foldxinity);
-                }
-              break;
-
-            case GEGl_FRACTAL_EXPLORER_TYPE_BARNSLEY_2:
-              foldxinitx = oldx * o->cx;
-              foldyinity = oldy * o->cy;
-              foldxinity = oldx * o->cy;
-              foldyinitx = oldy * o->cx;
-              /* orbit calculation */
-              if (foldxinity + foldyinitx >= 0)
-                {
-                  xx = foldxinitx - o->cx - foldyinity;
-                  y  = foldyinitx - o->cy + foldxinity;
-                }
-              else
-                {
-                  xx = foldxinitx + o->cx - foldyinity;
-                  y  = foldyinitx + o->cy + foldxinity;
-                }
-              break;
-
-            case GEGl_FRACTAL_EXPLORER_TYPE_BARNSLEY_3:
-              foldxinitx  = oldx * oldx;
-              foldyinity  = oldy * oldy;
-              foldxinity  = oldx * oldy;
-              /* orbit calculation */
-              if (oldx > 0)
-                {
-                  xx = foldxinitx - foldyinity - 1.0;
-                  y  = foldxinity * 2;
-                }
-              else
-                {
-                  xx = foldxinitx - foldyinity -1.0 + o->cx * oldx;
-                  y  = foldxinity * 2;
-                  y += o->cy * oldx;
-                }
-              break;
-
-            case GEGl_FRACTAL_EXPLORER_TYPE_SPIDER:
-              /* { c=z=pixel: z=z*z+c; c=c/2+z, |z|<=4 } */
-              xx = x*x - y*y + tmpx + o->cx;
-              y = 2 * oldx * oldy + tmpy + o->cy;
-              tmpx = tmpx/2 + xx;
-              tmpy = tmpy/2 + y;
-              break;
-
-            case GEGl_FRACTAL_EXPLORER_TYPE_MAN_O_WAR:
-              xx = x*x - y*y + tmpx + o->cx;
-              y = 2.0 * x * y + tmpy + o->cy;
-              tmpx = oldx;
-              tmpy = oldy;
-              break;
-
-            case GEGl_FRACTAL_EXPLORER_TYPE_LAMBDA:
-              tempsqrx = x * x;
-              tempsqry = y * y;
-              tempsqrx = oldx - tempsqrx + tempsqry;
-              tempsqry = -(oldy * oldx);
-              tempsqry += tempsqry + oldy;
-              xx = o->cx * tempsqrx - o->cy * tempsqry;
-              y = o->cx * tempsqry + o->cy * tempsqrx;
-              break;
-
-            case GEGl_FRACTAL_EXPLORER_TYPE_SIERPINSKI:
-              xx = oldx + oldx;
-              y = oldy + oldy;
-              if (oldy > .5)
-                y = y - 1;
-              else if (oldx > .5)
-                xx = xx - 1;
-              break;
-
-            default:
-              break;
-            }
-
-          x = xx;
-
-          if (((x * x) + (y * y)) >= 4.0)
-            break;
-        }
-
-      if (o->useloglog)
-        {
-          gdouble modulus_square = (x * x) + (y * y);
-
-          if (modulus_square > (G_E * G_E))
-              adjust = log (log (modulus_square) / 2.0) / log2;
-          else
-              adjust = 0.0;
-        }
-      else
-        {
-          adjust = 0.0;
-        }
-
-      color = (gint) (((counter - adjust) * (o->ncolors - 1)) / o->iter);
-
-      (*dest_row)[0] = colormap[color].r;
-      (*dest_row)[1] = colormap[color].g;
-      (*dest_row)[2] = colormap[color].b;
-      (*dest_row) += 3;
-    }
-}
+typedef gfloatRGB  clrmap[MAXNCOLORS];
 
 static void
 make_color_map (GeglChantO *o, clrmap colormap)
 {
   gint     i;
-  gint     r;
-  gint     gr;
-  gint     bl;
-  gdouble  redstretch;
-  gdouble  greenstretch;
-  gdouble  bluestretch;
-  gdouble  pi = atan (1) * 4;
-
-  redstretch   = o->redstretch * 127.5;
-  greenstretch = o->greenstretch * 127.5;
-  bluestretch  = o->bluestretch * 127.5;
+  gfloat   r;
+  gfloat   gr;
+  gfloat   bl;
 
   for (i = 0; i < o->ncolors; i++)
     {
@@ -324,13 +116,13 @@ make_color_map (GeglChantO *o, clrmap colormap)
       switch (o->redmode)
         {
         case GEGl_FRACTAL_EXPLORER_MODE_SIN:
-          r = (int) redstretch *(1.0 + sin((x - 1) * pi));
+          r = 0.5 * o->redstretch *(1.0 + sin((x - 1) * G_PI));
           break;
         case GEGl_FRACTAL_EXPLORER_MODE_COS:
-          r = (int) redstretch *(1.0 + cos((x - 1) * pi));
+          r = 0.5 * o->redstretch *(1.0 + cos((x - 1) * G_PI));
           break;
         case GEGl_FRACTAL_EXPLORER_MODE_NONE:
-          r = (int)(redstretch *(x));
+          r = 0.5 * o->redstretch * x;
           break;
         default:
           break;
@@ -339,13 +131,13 @@ make_color_map (GeglChantO *o, clrmap colormap)
       switch (o->greenmode)
         {
         case GEGl_FRACTAL_EXPLORER_MODE_SIN:
-          gr = (int) greenstretch *(1.0 + sin((x - 1) * pi));
+          gr = 0.5 * o->greenstretch *(1.0 + sin((x - 1) * G_PI));
           break;
         case GEGl_FRACTAL_EXPLORER_MODE_COS:
-          gr = (int) greenstretch *(1.0 + cos((x - 1) * pi));
+          gr = 0.5 * o->greenstretch *(1.0 + cos((x - 1) * G_PI));
           break;
         case GEGl_FRACTAL_EXPLORER_MODE_NONE:
-          gr = (int)(greenstretch *(x));
+          gr = 0.5 * o->greenstretch * x;
           break;
         default:
           break;
@@ -354,30 +146,26 @@ make_color_map (GeglChantO *o, clrmap colormap)
       switch (o->bluemode)
         {
         case GEGl_FRACTAL_EXPLORER_MODE_SIN:
-          bl = (int) bluestretch * (1.0 + sin ((x - 1) * pi));
+          bl = 0.5 * o->bluestretch * (1.0 + sin ((x - 1) * G_PI));
           break;
         case GEGl_FRACTAL_EXPLORER_MODE_COS:
-          bl = (int) bluestretch * (1.0 + cos ((x - 1) * pi));
+          bl = 0.5 * o->bluestretch * (1.0 + cos ((x - 1) * G_PI));
           break;
         case GEGl_FRACTAL_EXPLORER_MODE_NONE:
-          bl = (int) (bluestretch * x);
+          bl = 0.5 * o->bluestretch * x;
           break;
         default:
           break;
         }
 
-      r  = MIN (r,  255);
-      gr = MIN (gr, 255);
-      bl = MIN (bl, 255);
-
       if (o->redinvert)
-        r = 255 - r;
+        r = 1.0 - r;
 
       if (o->greeninvert)
-        gr = 255 - gr;
+        gr = 1.0 - gr;
 
       if (o->blueinvert)
-        bl = 255 - bl;
+        bl = 1.0 - bl;
 
       colormap[i].r = r;
       colormap[i].g = gr;
@@ -388,54 +176,203 @@ make_color_map (GeglChantO *o, clrmap colormap)
 static void
 prepare (GeglOperation *operation)
 {
-  gegl_operation_set_format (operation, "output", babl_format ("R'G'B' u8"));
+  gegl_operation_set_format (operation, "output", babl_format ("RGBA float"));
 }
 
 static GeglRectangle
 get_bounding_box (GeglOperation *operation)
 {
-  GeglChantO    *o = GEGL_CHANT_PROPERTIES (operation);
-  GeglRectangle  result = {0,0,0,0};
-
-  result.width  = o->width;
-  result.height = o->height;
-
-  return result;
+  return gegl_rectangle_infinite_plane ();
 }
 
 static gboolean
 process (GeglOperation       *operation,
-         GeglBuffer          *output,
-         const GeglRectangle *result,
+         void                *out_buf,
+         glong                n_pixels,
+         const GeglRectangle *roi,
          gint                 level)
 {
   GeglChantO *o = GEGL_CHANT_PROPERTIES (operation);
+  gfloat     *out_pixel = out_buf;
+  gint        pixelx = roi->x; /* initial x                   */
+  gint        pixely = roi->y; /*           and y coordinates */
+  gdouble     x,y;             /* coordinate in fractal space */
+  gdouble     a,b;             /* main fractal variable in iteration loop */
+  gdouble     nexta;
+  gdouble     tmpx, tmpy;
+  gdouble     foldxinitx;
+  gdouble     foldxinity;
+  gdouble     foldyinitx;
+  gdouble     foldyinity;
+  gdouble     tempsqrx;
+  gdouble     tempsqry;
+  gdouble     olda,oldb;
+  gdouble     adjust = 0.0;
+  gint        counter;         /* iteration counter */
+  gdouble     log2 = log (2.0);
+  gint        color;
+
   clrmap  colormap;
-  guchar *buf;
-  gint    pxsize;
 
   make_color_map (o, colormap);
 
-  g_object_get (output, "px-size", &pxsize, NULL);
-
-  buf  = g_new (guchar, result->width * result->height * pxsize);
+  while (n_pixels--)
     {
-      guchar *dst=buf;
-      gint y;
-      for (y=0; y < result->height; y++)
+      x = pixelx / 400.0; /* room for moving in the fractal space */
+      y = pixely / 400.0;
+
+      if (o->fractaltype == GEGl_FRACTAL_EXPLORER_TYPE_MANDELBROT)
         {
-          explorer_render_row (o,
-                               result->x,
-                               result->x + result->width ,
-                               result->y + y,
-                               colormap,
-                               &dst);
+          a = b = 0;
+        }
+      else
+        {
+          tmpx = a = x;
+          tmpy = b = y;
+        }
+
+      for (counter = 0; counter < o->iter; counter++)
+        {
+          olda = a;
+          oldb = b;
+
+          switch (o->fractaltype)
+            {
+            case GEGl_FRACTAL_EXPLORER_TYPE_MANDELBROT:
+              nexta = a * a - b * b + x;
+              b = 2.0 * a * b + y;
+              break;
+
+            case GEGl_FRACTAL_EXPLORER_TYPE_JULIA:
+              nexta = a * a - b * b + o->cx;
+              b = 2.0 * a * b + o->cy;
+              break;
+
+            case GEGl_FRACTAL_EXPLORER_TYPE_BARNSLEY_1:
+              foldxinitx = olda * o->cx;
+              foldyinity = oldb * o->cy;
+              foldxinity = olda * o->cy;
+              foldyinitx = oldb * o->cx;
+              /* orbit calculation */
+              if (olda >= 0)
+                {
+                  nexta = (foldxinitx - o->cx - foldyinity);
+                  b  = (foldyinitx - o->cy + foldxinity);
+                }
+              else
+                {
+                  nexta = (foldxinitx + o->cx - foldyinity);
+                  b  = (foldyinitx + o->cy + foldxinity);
+                }
+              break;
+
+            case GEGl_FRACTAL_EXPLORER_TYPE_BARNSLEY_2:
+              foldxinitx = olda * o->cx;
+              foldyinity = oldb * o->cy;
+              foldxinity = olda * o->cy;
+              foldyinitx = oldb * o->cx;
+              /* orbit calculation */
+              if (foldxinity + foldyinitx >= 0)
+                {
+                  nexta = foldxinitx - o->cx - foldyinity;
+                  b  = foldyinitx - o->cy + foldxinity;
+                }
+              else
+                {
+                  nexta = foldxinitx + o->cx - foldyinity;
+                  b  = foldyinitx + o->cy + foldxinity;
+                }
+              break;
+
+            case GEGl_FRACTAL_EXPLORER_TYPE_BARNSLEY_3:
+              foldxinitx  = olda * olda;
+              foldyinity  = oldb * oldb;
+              foldxinity  = olda * oldb;
+              /* orbit calculation */
+              if (olda > 0)
+                {
+                  nexta = foldxinitx - foldyinity - 1.0;
+                  b  = foldxinity * 2;
+                }
+              else
+                {
+                  nexta = foldxinitx - foldyinity -1.0 + o->cx * olda;
+                  b  = foldxinity * 2;
+                  b += o->cy * olda;
+                }
+              break;
+
+            case GEGl_FRACTAL_EXPLORER_TYPE_SPIDER:
+              /* { c=z=pixel: z=z*z+c; c=c/2+z, |z|<=4 } */
+              nexta = a*a - b*b + tmpx + o->cx;
+              b = 2 * olda * oldb + tmpy + o->cy;
+              tmpx = tmpx/2 + nexta;
+              tmpy = tmpy/2 + b;
+              break;
+
+            case GEGl_FRACTAL_EXPLORER_TYPE_MAN_O_WAR:
+              nexta = a*a - b*b + tmpx + o->cx;
+              b = 2.0 * a * b + tmpy + o->cy;
+              tmpx = olda;
+              tmpy = oldb;
+              break;
+
+            case GEGl_FRACTAL_EXPLORER_TYPE_LAMBDA:
+              tempsqrx = a * a;
+              tempsqry = b * b;
+              tempsqrx = olda - tempsqrx + tempsqry;
+              tempsqry = -(oldb * olda);
+              tempsqry += tempsqry + oldb;
+              nexta = o->cx * tempsqrx - o->cy * tempsqry;
+              b = o->cx * tempsqry + o->cy * tempsqrx;
+              break;
+
+            case GEGl_FRACTAL_EXPLORER_TYPE_SIERPINSKI:
+              nexta = olda + olda;
+              b = oldb + oldb;
+              if (oldb > .5)
+                b = b - 1;
+              else if (olda > .5)
+                nexta = nexta - 1;
+              break;
+
+            default:
+              break;
+            }
+
+          a = nexta;
+
+          if (((a * a) + (b * b)) >= 4.0)
+            break;
+        }
+
+      if (o->useloglog)
+        {
+          gdouble modulus_square = (a * a) + (b * b);
+
+          if (modulus_square > (G_E * G_E))
+              adjust = log (log (modulus_square) / 2.0) / log2;
+          else
+              adjust = 0.0;
+        }
+
+      color = (gint) (((counter - adjust) * (o->ncolors - 1)) / o->iter);
+
+      out_pixel[0] = colormap[color].r;
+      out_pixel[1] = colormap[color].g;
+      out_pixel[2] = colormap[color].b;
+      out_pixel[3] = 1.0;
+
+      out_pixel += 4;
+
+      /* update x and y coordinates */
+      pixelx++;
+      if (pixelx>=roi->x + roi->width)
+        {
+          pixelx=roi->x;
+          pixely++;
         }
     }
-
-  gegl_buffer_set (output, NULL, 0, babl_format ("R'G'B' u8"), buf,
-                   GEGL_AUTO_ROWSTRIDE);
-  g_free (buf);
 
   return TRUE;
 }
@@ -444,13 +381,13 @@ process (GeglOperation       *operation,
 static void
 gegl_chant_class_init (GeglChantClass *klass)
 {
-  GeglOperationClass       *operation_class;
-  GeglOperationSourceClass *source_class;
+  GeglOperationClass            *operation_class;
+  GeglOperationPointRenderClass *point_render_class;
 
-  operation_class = GEGL_OPERATION_CLASS (klass);
-  source_class    = GEGL_OPERATION_SOURCE_CLASS (klass);
+  operation_class    = GEGL_OPERATION_CLASS (klass);
+  point_render_class = GEGL_OPERATION_POINT_RENDER_CLASS (klass);
 
-  source_class->process = process;
+  point_render_class->process = process;
   operation_class->get_bounding_box = get_bounding_box;
   operation_class->prepare = prepare;
 
@@ -459,9 +396,6 @@ gegl_chant_class_init (GeglChantClass *klass)
     "categories" , "render",
     "description", _("Fractal Explorer"),
     NULL);
-
-  operation_class->no_cache = TRUE;
-  operation_class->get_cached_region = NULL;
 }
 
 #endif
