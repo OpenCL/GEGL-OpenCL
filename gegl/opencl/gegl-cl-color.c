c7fe127edd549af4a336becfd8b9e0e4eb82012a
Victor Oliveira <victormatheus@gmail.com>
2012-06-17 17:52:36 -0700

    GNU headers in OpenCL sources\

--- gegl/opencl/gegl-cl-color.c
+++ gegl/opencl/gegl-cl-color.c
@@ -1,4 +1,23 @@
+/* This file is part of GEGL
+ *
+ * GEGL is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * GEGL is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GEGL; if not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2012 Victor Oliveira (victormatheus@gmail.com)
+ */
+
 #include "config.h"
+
 #include "gegl.h"
 #include "gegl/gegl-debug.h"
 #include "gegl-cl-color.h"
