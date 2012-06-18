4de7509e21ac13310b19c3322ba6a9aceaf5e54c
Massimo Valentini <mvalentini@src.gnome.org>
2012-06-18 19:38:12 +0200

    gegl: set gegl-config default chunk-size to 512 * 512
    otherwise make check fails for tests/compositions/clones.xml

--- gegl/gegl-config.c
+++ gegl/gegl-config.c
@@ -225,7 +225,7 @@ gegl_config_class_init (GeglConfigClass *klass)
                                    g_param_spec_int ("chunk-size",
                                                      "Chunk size",
                                                      "the number of pixels processed simultaneously by GEGL.",
-                                                     1, G_MAXINT, 256 * 300,
+                                                     1, G_MAXINT, 512 * 512,
                                                      G_PARAM_READWRITE |
                                                      G_PARAM_CONSTRUCT));
 
