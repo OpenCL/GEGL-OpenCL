d48d9c55ab1da889c39ca552696601a726600d71
Michael Natterer <mitch@gimp.org>
2013-03-08 15:39:13 +0100

    operations: change noise-hurl's formats to "R'G'B'A float"
    because it produces random RGB values that need to be evenly
    distributed perceptually, not linearly.

--- operations/common/noise-hurl.c
+++ operations/common/noise-hurl.c
@@ -47,8 +47,8 @@ gegl_chant_int    (repeat, _("Repeat"),
 static void
 prepare (GeglOperation *operation)
 {
-  gegl_operation_set_format (operation, "input" , babl_format ("RGBA float"));
-  gegl_operation_set_format (operation, "output", babl_format ("RGBA float"));
+  gegl_operation_set_format (operation, "input" , babl_format ("R'G'B'A float"));
+  gegl_operation_set_format (operation, "output", babl_format ("R'G'B'A float"));
 }
 
 static gboolean
