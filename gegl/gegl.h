f087a7ca8b97d2afbe4471e9823d408a5c96e8ac
Daniel Sabo <DanielSabo@gmail.com>
2013-06-02 20:31:35 -0700

    Add doc comments for the gegl-apply.c functions

--- gegl/gegl.h
+++ gegl/gegl.h
@@ -836,29 +836,94 @@ GeglConfig      * gegl_config (void);
  */
 void gegl_load_module_directory (const gchar *path);
 
-
+/**
+ * gegl_apply_op:
+ * @buffer: the #GeglBuffer to apply onto
+ * @operation_name: name of the operation to apply
+ * @...: the settings for the operation. Zero or more key/value pairs,
+ * ended terminated with NULL.
+ *
+ * Apply the operation to buffer, overwritting the contents of buffer.
+ *
+ */
 void        gegl_apply_op  (GeglBuffer    *buffer,
                             const gchar   *operation_name,
                             ...) G_GNUC_NULL_TERMINATED;
 
+/**
+ * gegl_filter_op:
+ * @source_buffer: the source #GeglBuffer for the filter
+ * @operation_name: name of the operation to apply
+ * @...: the settings for the operation. Zero or more key/value pairs,
+ * ended terminated with NULL.
+ *
+ * Apply the operation to source_buffer, returning the result in a new buffer.
+ *
+ * Return value: (transfer full): the result of the filter
+ */
 GeglBuffer *gegl_filter_op (GeglBuffer    *source_buffer,
                             const gchar   *operation_name,
                             ...) G_GNUC_NULL_TERMINATED;
 
+
+/**
+ * gegl_render_op:
+ * @source_buffer: the source #GeglBuffer for the filter
+ * @target_buffer: the source #GeglBuffer for the filter
+ * @operation_name: name of the operation to apply
+ * @...: the settings for the operation. Zero or more key/value pairs,
+ * ended terminated with NULL.
+ *
+ * Apply the operation to source_buffer, writing the results to target_buffer.
+ *
+ */
 void        gegl_render_op (GeglBuffer    *source_buffer,
                             GeglBuffer    *target_buffer,
                             const gchar   *operation_name,
                             ...) G_GNUC_NULL_TERMINATED;
 
-/* the following only exist to make gegl_apply nad gegl_filter bindable */
+/* the following only exist to make gegl_apply and gegl_filter bindable */
+
+/**
+ * gegl_apply_op_valist:
+ * @buffer: the #GeglBuffer to apply onto
+ * @operation_name: name of the operation to apply
+ * @var_args: the settings for the operation. Zero or more key/value pairs,
+ * ended terminated with NULL.
+ *
+ * Apply the operation to buffer, overwritting the contents of buffer.
+ *
+ */
 void        gegl_apply_op_valist (GeglBuffer    *buffer,
                                   const gchar   *operation_name,
                                   va_list        var_args);
 
+/**
+ * gegl_filter_op_valist:
+ * @source_buffer: the source #GeglBuffer for the filter
+ * @operation_name: name of the operation to apply
+ * @var_args: the settings for the operation. Zero or more key/value pairs,
+ * ended terminated with NULL.
+ *
+ * Apply the operation to source_buffer, returning the result in a new buffer.
+ *
+ * Return value: (transfer full): the result of the filter
+ */
 GeglBuffer *gegl_filter_op_valist (GeglBuffer   *source_buffer,
                                    const gchar  *operation_name,
                                    va_list       var_args);
 
+/**
+ * gegl_render_op_valist:
+ * @source_buffer: the source #GeglBuffer for the filter
+ * @target_buffer: the source #GeglBuffer for the filter
+ * @operation_name: name of the operation to apply
+ * @var_args: the settings for the operation. Zero or more key/value pairs,
+ * ended terminated with NULL.
+ *
+ * Apply the operation to source_buffer, writing the results to target_buffer.
+ *
+ */
 void        gegl_render_op_valist (GeglBuffer   *source_buffer,
                                    GeglBuffer   *target_buffer,
                                    const gchar  *operation_name,
