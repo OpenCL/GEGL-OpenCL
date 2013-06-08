d41e1ae5d7969b44be6708d95f997b1b2df3f385
Daniel Sabo <DanielSabo@gmail.com>
2013-06-08 09:05:28 -0700

    Clean up gegl-graph-traversal.h

--- gegl/process/gegl-graph-traversal.h
+++ gegl/process/gegl-graph-traversal.h
@@ -1,17 +1,35 @@
+/* This file is part of GEGL.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with GEGL; if not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2013 Daniel Sabo
+ */
 #ifndef __GEGL_GRAPH_TRAVERSAL_H__
 #define __GEGL_GRAPH_TRAVERSAL_H__
 
 typedef struct _GeglGraphTraversal GeglGraphTraversal;
 
-void gegl_graph_dfs_dump (GeglNode *);
+GeglGraphTraversal *gegl_graph_build            (GeglNode            *node);
+void                gegl_graph_rebuild          (GeglGraphTraversal  *path,
+                                                 GeglNode            *node);
+void                gegl_graph_free             (GeglGraphTraversal  *path);
 
-GeglGraphTraversal *gegl_graph_build (GeglNode *node);
-void gegl_graph_rebuild (GeglGraphTraversal *path, GeglNode *node);
-void gegl_graph_free (GeglGraphTraversal *path);
+void                gegl_graph_prepare          (GeglGraphTraversal  *path);
+void                gegl_graph_prepare_request  (GeglGraphTraversal  *path,
+                                                 const GeglRectangle *roi);
+GeglBuffer         *gegl_graph_process          (GeglGraphTraversal  *path);
 
-void gegl_graph_prepare (GeglGraphTraversal *path);
-void gegl_graph_prepare_request (GeglGraphTraversal *path, const GeglRectangle *roi);
-GeglBuffer *gegl_graph_process (GeglGraphTraversal *path);
-GeglRectangle gegl_graph_get_bounding_box (GeglGraphTraversal *path);
+GeglRectangle       gegl_graph_get_bounding_box (GeglGraphTraversal  *path);
 
 #endif /* __GEGL_GRAPH_TRAVERSAL_H__ */
