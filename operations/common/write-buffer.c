dc351497870b40faf53cf22785255b7d7af87466
Ville Sokk <ville.sokk@gmail.com>
2013-07-06 21:29:19 +0300

    operations: remove flush parameter from write-buffer. It was a stop-gap solution.

--- operations/common/write-buffer.c
+++ operations/common/write-buffer.c
@@ -25,9 +25,6 @@
 gegl_chant_object (buffer, _("Buffer location"),
                    _("Write to an existing GeglBuffer"))
 
-gegl_chant_boolean (flush, _("Flush buffer"), TRUE,
-                    _("Flush buffer after writing"))
-
 #else
 
 #define GEGL_CHANT_TYPE_SINK
@@ -111,8 +108,7 @@ process (GeglOperation       *operation,
       else
         gegl_buffer_copy (input, result, output, result);
 
-      if (o->flush)
-        gegl_buffer_flush (output);
+      gegl_buffer_flush (output);
 
       gegl_node_emit_computed (operation->node, result);
     }
