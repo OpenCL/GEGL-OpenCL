0c70eacf2ea8d5f2bb2b648098e7b5bd20cc3e31
Daniel Sabo <DanielSabo@gmail.com>
2013-10-20 02:24:04 -0700

    Always call g_warning when CL_ERROR is called

--- gegl/opencl/gegl-cl.h
+++ gegl/opencl/gegl-cl.h
@@ -26,17 +26,7 @@
 
 cl_int gegl_cl_set_kernel_args (cl_kernel kernel, ...) G_GNUC_NULL_TERMINATED;
 
-#ifdef __GEGL_DEBUG_H__
-
-#define CL_ERROR {GEGL_NOTE (GEGL_DEBUG_OPENCL, "Error in %s:%d@%s - %s\n", __FILE__, __LINE__, __func__, gegl_cl_errstring(cl_err)); goto error;}
-
-#else
-
-/* public header for gegl ops */
-
 #define CL_ERROR {g_warning("Error in %s:%d@%s - %s\n", __FILE__, __LINE__, __func__, gegl_cl_errstring(cl_err)); goto error;}
-
-#endif
 
 #define CL_CHECK {if (cl_err != CL_SUCCESS) CL_ERROR;}
 
