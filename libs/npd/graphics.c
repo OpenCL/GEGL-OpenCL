c99570fea158736ab0aa5af7c258f27f4345562f
Marek Dvoroznak <dvoromar@gmail.com>
2013-12-02 02:43:38 +0100

    npd: mark more functions as static

--- libs/npd/graphics.c
+++ libs/npd/graphics.c
@@ -27,12 +27,329 @@
 #include <stdio.h>
 #include <string.h>
 
+static gfloat
+npd_bilinear_interpolation (gfloat I0,
+                            gfloat I1,
+                            gfloat I2,
+                            gfloat I3,
+                            gfloat dx,
+                            gfloat dy)
+{
+  return (I0 * (1 - dx) + I1 * dx) * (1 - dy) +
+         (I2 * (1 - dx) + I3 * dx) *      dy;
+}
+
+static void
+npd_bilinear_color_interpolation (NPDColor *I0,
+                                  NPDColor *I1,
+                                  NPDColor *I2,
+                                  NPDColor *I3,
+                                  gfloat    dx,
+                                  gfloat    dy,
+                                  NPDColor *out)
+{
+  out->r = npd_bilinear_interpolation (I0->r, I1->r, I2->r, I3->r, dx, dy);
+  out->g = npd_bilinear_interpolation (I0->g, I1->g, I2->g, I3->g, dx, dy);
+  out->b = npd_bilinear_interpolation (I0->b, I1->b, I2->b, I3->b, dx, dy);
+  out->a = npd_bilinear_interpolation (I0->a, I1->a, I2->a, I3->a, dx, dy);
+}
+
+static gfloat
+npd_blend_band (gfloat src,
+                gfloat dst,
+                gfloat src_alpha,
+                gfloat dst_alpha,
+                gfloat out_alpha_recip)
+{
+  return (src * src_alpha +
+          dst * dst_alpha * (1 - src_alpha)) * out_alpha_recip;
+}
+
+static void
+npd_blend_colors (NPDColor *src,
+                  NPDColor *dst,
+                  NPDColor *out_color)
+{
+#ifdef NPD_RGBA_FLOAT
+  gfloat src_A = src->a,
+         dst_A = dst->a;
+#else
+  gfloat src_A = src->a / 255.0,
+         dst_A = dst->a / 255.0;
+#endif
+  gfloat out_alpha = src_A + dst_A * (1 - src_A);
+  gfloat out_alpha_recip = 1 / out_alpha;
+
+  out_color->r = npd_blend_band (src->r, dst->r, src_A, dst_A, out_alpha_recip);
+  out_color->g = npd_blend_band (src->g, dst->g, src_A, dst_A, out_alpha_recip);
+  out_color->b = npd_blend_band (src->b, dst->b, src_A, dst_A, out_alpha_recip);
+#ifdef NPD_RGBA_FLOAT
+  out_color->a = out_alpha;
+#else
+  out_color->a = out_alpha * 255;
+#endif
+}
+
+static void
+npd_draw_texture_line (gint        x1,
+                       gint        x2,
+                       gint        y,
+                       NPDMatrix  *A,
+                       NPDImage   *input_image,
+                       NPDImage   *output_image,
+                       NPDSettings settings)
+{
+  gint x, fx, fy;
+  gfloat dx, dy;
+
+  for (x = x1; x <= x2; x++)
+    {
+      NPDPoint p, q;
+      NPDColor I0, interpolated, *final;
+
+      q.x = x; q.y = y;
+      npd_apply_transformation (A, &q, &p);
+
+      fx = floor (p.x);
+      fy = floor (p.y);
+
+      npd_get_pixel_color (input_image, fx, fy, &I0);
+      final = &I0;
+
+      /* bilinear interpolation */
+      if (settings & NPD_BILINEAR_INTERPOLATION)
+        {
+          NPDColor I1, I2, I3;
+
+          dx =  p.x - fx;
+          dy =  p.y - fy;
+
+          npd_get_pixel_color (input_image, fx + 1, fy,     &I1);
+          npd_get_pixel_color (input_image, fx,     fy + 1, &I2);
+          npd_get_pixel_color (input_image, fx + 1, fy + 1, &I3);
+          npd_bilinear_color_interpolation (&I0, &I1, &I2, &I3, dx, dy, &interpolated);
+          final = &interpolated;
+        }
+
+      /* alpha blending */
+      if (settings & NPD_ALPHA_BLENDING)
+        {
+          NPDColor dest;
+          npd_get_pixel_color (output_image, x, y, &dest);
+          npd_blend_colors (final, &dest, final);
+        }
+
+      npd_set_pixel_color (output_image, x, y, final);
+    }
+}
+
+static void
+npd_texture_fill_triangle (gint       x1,
+                           gint       y1,
+                           gint       x2,
+                           gint       y2,
+                           gint       x3,
+                           gint       y3,
+                           NPDMatrix *A,
+                           NPDImage  *input_image,
+                           NPDImage  *output_image,
+                           NPDSettings settings)
+{
+  gint yA, yB, yC, xA, xB, xC;
+  gint tmp, y;
+  gint deltaXAB, deltaYAB;
+  gint deltaXBC, deltaYBC;
+  gint deltaXAC, deltaYAC;
+
+  gfloat slopeAB;
+  gfloat slopeBC;
+  gfloat slopeAC;
+
+  gfloat k, l;
+  gfloat slope1, slope2, slope3, slope4;
+
+  if (y1 == y2 && x1 > x2)
+    {
+      tmp = y1; y1 = y2; y2 = tmp;
+      tmp = x1; x1 = x2; x2 = tmp;
+    }
+  if (y1 == y3 && x1 > x3)
+    {
+      tmp = y1; y1 = y3; y3 = tmp;
+      tmp = x1; x1 = x3; x3 = tmp;
+    }
+  if (y2 == y3 && x2 > x3)
+    {
+      tmp = y2; y2 = y3; y3 = tmp;
+      tmp = x2; x2 = x3; x3 = tmp;
+    }
+
+  if (y1 <= y2)
+    {
+      if (y2 <= y3)
+        {
+          /* y1 <= y2 <= y3 */
+          yA = y1; yB = y2; yC = y3;
+          xA = x1; xB = x2; xC = x3;
+        }
+      else if (y1 <= y3)
+        {
+          /* y1 <= y3 < y2 */
+          yA = y1; yB = y3; yC = y2;
+          xA = x1; xB = x3; xC = x2;
+        }
+      else
+        {
+          /* y3 < y1 < y2 */
+          yA = y3; yB = y1; yC = y2;
+          xA = x3; xB = x1; xC = x2;
+        }
+    }
+  else
+    {
+      if (y1 <= y3)
+        {
+          /* y2 < y1 <= y3 */
+          yA = y2; yB = y1; yC = y3;
+          xA = x2; xB = x1; xC = x3;
+        }
+      else if (y2 <= y3)
+        {
+          /* y2 <= y3 < y1 */
+          yA = y2; yB = y3; yC = y1;
+          xA = x2; xB = x3; xC = x1;
+        }
+      else
+        {
+          /* y3 < y2 < y1 */
+          yA = y3; yB = y2; yC = y1;
+          xA = x3; xB = x2; xC = x1;
+        }
+    }
+
+  deltaXAB = xB - xA, deltaYAB = yB - yA;
+  deltaXBC = xC - xB, deltaYBC = yC - yB;
+  deltaXAC = xC - xA, deltaYAC = yC - yA;
+
+  slopeBC = (deltaYBC == 0 ? 0 : (gfloat) deltaXBC / deltaYBC);
+  slopeAC = (deltaYAC == 0 ? 0 : (gfloat) deltaXAC / deltaYAC);
+
+  if (deltaYAB == 0)
+    {
+      slopeAB = 0;
+      k = xA;
+      l = xB;
+
+      slope1 = slopeAB;
+      slope2 = slopeAC;
+      slope3 = slopeAC;
+      slope4 = slopeBC;
+    }
+  else
+    {
+      slopeAB = (gfloat) deltaXAB / deltaYAB;
+      k = xA;
+      l = xA;
+
+      if (slopeAB > slopeAC)
+        {
+          slope1 = slopeAC;
+          slope2 = slopeAB;
+          slope3 = slopeAC;
+          slope4 = slopeBC;
+        }
+      else
+        {
+          slope1 = slopeAB;
+          slope2 = slopeAC;
+          slope3 = slopeBC;
+          slope4 = slopeAC;
+        }
+    }
+
+  for (y = yA; y < yB; y++)
+    {
+      npd_draw_texture_line ((gint) round (k), (gint) round (l),
+                             y, A,
+                             input_image, output_image,
+                             settings);
+      k += slope1;
+      l += slope2;
+    }
+
+  for (y = yB; y <= yC; y++)
+    {
+      npd_draw_texture_line ((gint) round (k), (gint) round (l),
+                             y, A,
+                             input_image, output_image,
+                             settings);
+      k += slope3;
+      l += slope4;
+    }
+}
+
+static void
+npd_texture_quadrilateral (NPDBone    *reference_bone,
+                           NPDBone    *current_bone,
+                           NPDImage   *input_image,
+                           NPDImage   *output_image,
+                           NPDSettings settings)
+{
+  /* p1 are points of domain, p2 are points of codomain */
+  NPDPoint *p1 = current_bone->points;
+  NPDPoint *p2 = reference_bone->points;
+
+  NPDMatrix A;
+
+  npd_compute_affinity (&p1[0], &p1[1], &p1[2],
+                        &p2[0], &p2[1], &p2[2], &A);
+  npd_texture_fill_triangle ((gint) p1[0].x, (gint) p1[0].y,
+                             (gint) p1[1].x, (gint) p1[1].y,
+                             (gint) p1[2].x, (gint) p1[2].y,
+                             &A, input_image, output_image,
+                             settings);
+
+  npd_compute_affinity (&p1[0], &p1[2], &p1[3],
+                        &p2[0], &p2[2], &p2[3], &A);
+  npd_texture_fill_triangle ((gint) p1[0].x, (gint) p1[0].y,
+                             (gint) p1[2].x, (gint) p1[2].y,
+                             (gint) p1[3].x, (gint) p1[3].y,
+                             &A, input_image, output_image,
+                             settings);
+}
+
 void
-npd_create_mesh_from_image (NPDModel *model,
-                            gint      width,
-                            gint      height,
-                            gint      position_x,
-                            gint      position_y)
+npd_draw_model_into_image (NPDModel *model,
+                           NPDImage *image)
+{
+  NPDHiddenModel *hm = model->hidden_model;
+  gint i;
+
+  for (i = 0; i < hm->num_of_bones; i++)
+    {
+      npd_texture_quadrilateral (&hm->reference_bones[i],
+                                 &hm->current_bones[i],
+                                  model->reference_image,
+                                  image,
+                                  NPD_BILINEAR_INTERPOLATION | NPD_ALPHA_BLENDING);
+    }
+}
+
+gboolean
+npd_is_color_transparent (NPDColor *color)
+{
+  if (npd_equal_floats (color->a, 0.0))
+    return TRUE;
+
+  return FALSE;
+}
+
+static void
+npd_create_mesh (NPDModel *model,
+                 gint      width,
+                 gint      height,
+                 gint      position_x,
+                 gint      position_y)
 {
   gint      square_size = model->mesh_square_size;
   NPDImage *image = model->reference_image;
@@ -203,8 +520,8 @@ npd_create_mesh_from_image (NPDModel *model,
         }
     }
 
-/*
-// could be useful later
+#if 0
+  /* could be useful later */
   gint j;
   for (i = 0; i < hm->num_of_overlapping_points; i++)
   for (j = 0; j < hm->list_of_overlapping_points[i].num_of_points; j++)
@@ -213,8 +530,25 @@ npd_create_mesh_from_image (NPDModel *model,
       p->overlapping_points = &hm->list_of_overlapping_points[i];
       p->counterpart->overlapping_points = &hm->list_of_overlapping_points[i];
     }
-*/
+#endif
 }
+
+void
+npd_create_model_from_image (NPDModel  *model,
+                             NPDImage  *image,
+                             gint       width,
+                             gint       height,
+                             gint       position_x,
+                             gint       position_y,
+                             gint       square_size)
+{
+  npd_init_model (model);
+  model->reference_image = image;
+  model->mesh_square_size = square_size;
+
+  npd_create_mesh (model, width, height, position_x, position_y);
+}
+
 
 void
 npd_draw_mesh (NPDModel   *model,
@@ -237,317 +571,4 @@ npd_draw_mesh (NPDModel   *model,
         }
       npd_draw_line (display, p1->x, p1->y, first->x, first->y);
     }
-}
-
-gfloat
-npd_bilinear_interpolation (gfloat I0,
-                            gfloat I1,
-                            gfloat I2,
-                            gfloat I3,
-                            gfloat dx,
-                            gfloat dy)
-{
-  return (I0 * (1 - dx) + I1 * dx) * (1 - dy) +
-         (I2 * (1 - dx) + I3 * dx) *      dy;
-}
-
-void
-npd_bilinear_color_interpolation (NPDColor *I0,
-                                  NPDColor *I1,
-                                  NPDColor *I2,
-                                  NPDColor *I3,
-                                  gfloat    dx,
-                                  gfloat    dy,
-                                  NPDColor *out)
-{
-  out->r = npd_bilinear_interpolation (I0->r, I1->r, I2->r, I3->r, dx, dy);
-  out->g = npd_bilinear_interpolation (I0->g, I1->g, I2->g, I3->g, dx, dy);
-  out->b = npd_bilinear_interpolation (I0->b, I1->b, I2->b, I3->b, dx, dy);
-  out->a = npd_bilinear_interpolation (I0->a, I1->a, I2->a, I3->a, dx, dy);
-}
-
-gfloat
-npd_blend_band (gfloat src,
-                gfloat dst,
-                gfloat src_alpha,
-                gfloat dst_alpha,
-                gfloat out_alpha_recip)
-{
-  return (src * src_alpha +
-          dst * dst_alpha * (1 - src_alpha)) * out_alpha_recip;
-}
-
-void
-npd_blend_colors (NPDColor *src,
-                  NPDColor *dst,
-                  NPDColor *out_color)
-{
-#ifdef NPD_RGBA_FLOAT
-  gfloat src_A = src->a,
-         dst_A = dst->a;
-#else
-  gfloat src_A = src->a / 255.0,
-         dst_A = dst->a / 255.0;
-#endif
-  gfloat out_alpha = src_A + dst_A * (1 - src_A);
-  gfloat out_alpha_recip = 1 / out_alpha;
-
-  out_color->r = npd_blend_band (src->r, dst->r, src_A, dst_A, out_alpha_recip);
-  out_color->g = npd_blend_band (src->g, dst->g, src_A, dst_A, out_alpha_recip);
-  out_color->b = npd_blend_band (src->b, dst->b, src_A, dst_A, out_alpha_recip);
-#ifdef NPD_RGBA_FLOAT
-  out_color->a = out_alpha;
-#else
-  out_color->a = out_alpha * 255;
-#endif
-}
-
-void
-npd_texture_fill_triangle (gint       x1,
-                           gint       y1,
-                           gint       x2,
-                           gint       y2,
-                           gint       x3,
-                           gint       y3,
-                           NPDMatrix *A,
-                           NPDImage  *input_image,
-                           NPDImage  *output_image,
-                           NPDSettings settings)
-{
-  gint yA, yB, yC, xA, xB, xC;
-  gint tmp, y;
-  gint deltaXAB, deltaYAB;
-  gint deltaXBC, deltaYBC;
-  gint deltaXAC, deltaYAC;
-
-  gfloat slopeAB;
-  gfloat slopeBC;
-  gfloat slopeAC;
-
-  gfloat k, l;
-  gfloat slope1, slope2, slope3, slope4;
-
-  if (y1 == y2 && x1 > x2)
-    {
-      tmp = y1; y1 = y2; y2 = tmp;
-      tmp = x1; x1 = x2; x2 = tmp;
-    }
-  if (y1 == y3 && x1 > x3)
-    {
-      tmp = y1; y1 = y3; y3 = tmp;
-      tmp = x1; x1 = x3; x3 = tmp;
-    }
-  if (y2 == y3 && x2 > x3)
-    {
-      tmp = y2; y2 = y3; y3 = tmp;
-      tmp = x2; x2 = x3; x3 = tmp;
-    }
-
-  if (y1 <= y2)
-    {
-      if (y2 <= y3)
-        {
-          /* y1 <= y2 <= y3 */
-          yA = y1; yB = y2; yC = y3;
-          xA = x1; xB = x2; xC = x3;
-        }
-      else if (y1 <= y3)
-        {
-          /* y1 <= y3 < y2 */
-          yA = y1; yB = y3; yC = y2;
-          xA = x1; xB = x3; xC = x2;
-        }
-      else
-        {
-          /* y3 < y1 < y2 */
-          yA = y3; yB = y1; yC = y2;
-          xA = x3; xB = x1; xC = x2;
-        }
-    }
-  else
-    {
-      if (y1 <= y3)
-        {
-          /* y2 < y1 <= y3 */
-          yA = y2; yB = y1; yC = y3;
-          xA = x2; xB = x1; xC = x3;
-        }
-      else if (y2 <= y3)
-        {
-          /* y2 <= y3 < y1 */
-          yA = y2; yB = y3; yC = y1;
-          xA = x2; xB = x3; xC = x1;
-        }
-      else
-        {
-          /* y3 < y2 < y1 */
-          yA = y3; yB = y2; yC = y1;
-          xA = x3; xB = x2; xC = x1;
-        }
-    }
-
-  deltaXAB = xB - xA, deltaYAB = yB - yA;
-  deltaXBC = xC - xB, deltaYBC = yC - yB;
-  deltaXAC = xC - xA, deltaYAC = yC - yA;
-
-  slopeBC = (deltaYBC == 0 ? 0 : (gfloat) deltaXBC / deltaYBC);
-  slopeAC = (deltaYAC == 0 ? 0 : (gfloat) deltaXAC / deltaYAC);
-
-  if (deltaYAB == 0)
-    {
-      slopeAB = 0;
-      k = xA;
-      l = xB;
-
-      slope1 = slopeAB;
-      slope2 = slopeAC;
-      slope3 = slopeAC;
-      slope4 = slopeBC;
-    }
-  else
-    {
-      slopeAB = (gfloat) deltaXAB / deltaYAB;
-      k = xA;
-      l = xA;
-
-      if (slopeAB > slopeAC)
-        {
-          slope1 = slopeAC;
-          slope2 = slopeAB;
-          slope3 = slopeAC;
-          slope4 = slopeBC;
-        }
-      else
-        {
-          slope1 = slopeAB;
-          slope2 = slopeAC;
-          slope3 = slopeBC;
-          slope4 = slopeAC;
-        }
-    }
-
-  for (y = yA; y < yB; y++)
-    {
-      npd_draw_texture_line ((gint) round (k), (gint) round (l),
-                             y, A,
-                             input_image, output_image,
-                             settings);
-      k += slope1;
-      l += slope2;
-    }
-
-  for (y = yB; y <= yC; y++)
-    {
-      npd_draw_texture_line ((gint) round (k), (gint) round (l),
-                             y, A,
-                             input_image, output_image,
-                             settings);
-      k += slope3;
-      l += slope4;
-    }
-}
-
-void
-npd_texture_quadrilateral (NPDBone    *reference_bone,
-                           NPDBone    *current_bone,
-                           NPDImage   *input_image,
-                           NPDImage   *output_image,
-                           NPDSettings settings)
-{
-  /* p1 are points of domain, p2 are points of codomain */
-  NPDPoint *p1 = current_bone->points;
-  NPDPoint *p2 = reference_bone->points;
-
-  NPDMatrix A;
-
-  npd_compute_affinity (&p1[0], &p1[1], &p1[2],
-                        &p2[0], &p2[1], &p2[2], &A);
-  npd_texture_fill_triangle ((gint) p1[0].x, (gint) p1[0].y,
-                             (gint) p1[1].x, (gint) p1[1].y,
-                             (gint) p1[2].x, (gint) p1[2].y,
-                             &A, input_image, output_image,
-                             settings);
-
-  npd_compute_affinity (&p1[0], &p1[2], &p1[3],
-                        &p2[0], &p2[2], &p2[3], &A);
-  npd_texture_fill_triangle ((gint) p1[0].x, (gint) p1[0].y,
-                             (gint) p1[2].x, (gint) p1[2].y,
-                             (gint) p1[3].x, (gint) p1[3].y,
-                             &A, input_image, output_image,
-                             settings);
-}
-
-void
-npd_draw_texture_line (gint        x1,
-                       gint        x2,
-                       gint        y,
-                       NPDMatrix  *A,
-                       NPDImage   *input_image,
-                       NPDImage   *output_image,
-                       NPDSettings settings)
-{
-  gint x, fx, fy;
-  gfloat dx, dy;
-  
-  for (x = x1; x <= x2; x++)
-    {
-      NPDPoint p, q;
-      NPDColor I0, interpolated, *final;
-
-      q.x = x; q.y = y;
-      npd_apply_transformation (A, &q, &p);
-
-      fx = floor (p.x);
-      fy = floor (p.y);
-
-      npd_get_pixel_color (input_image, fx, fy, &I0);
-      final = &I0;
-
-      /* bilinear interpolation */
-      if (settings & NPD_BILINEAR_INTERPOLATION)
-        {
-          NPDColor I1, I2, I3;
-
-          dx =  p.x - fx;
-          dy =  p.y - fy;
-
-          npd_get_pixel_color (input_image, fx + 1, fy,     &I1);
-          npd_get_pixel_color (input_image, fx,     fy + 1, &I2);
-          npd_get_pixel_color (input_image, fx + 1, fy + 1, &I3);
-          npd_bilinear_color_interpolation (&I0, &I1, &I2, &I3, dx, dy, &interpolated);
-          final = &interpolated;
-        }
-
-      /* alpha blending */
-      if (settings & NPD_ALPHA_BLENDING)
-        {
-          NPDColor dest;
-          npd_get_pixel_color (output_image, x, y, &dest);
-          npd_blend_colors (final, &dest, final);
-        }
-
-      npd_set_pixel_color (output_image, x, y, final);
-    }
-}
-
-gboolean
-npd_compare_colors (NPDColor *c1,
-                    NPDColor *c2)
-{
-  if (npd_equal_floats (c1->r, c2->r) &&
-      npd_equal_floats (c1->g, c2->g) &&
-      npd_equal_floats (c1->b, c2->b) &&
-      npd_equal_floats (c1->a, c2->a))
-    return TRUE;
-
-  return FALSE;
-}
-
-gboolean
-npd_is_color_transparent (NPDColor *color)
-{
-  if (npd_equal_floats (color->a, 0.0))
-    return TRUE;
-
-  return FALSE;
 }
--- libs/npd/graphics.h
+++ libs/npd/graphics.h
@@ -48,67 +48,21 @@ typedef enum
 
 void        npd_create_model_from_image       (NPDModel   *model,
                                                NPDImage   *image,
-                                               gint        square_size);
-void        npd_create_mesh_from_image        (NPDModel   *model,
                                                gint        width,
                                                gint        height,
                                                gint        position_x,
-                                               gint        position_y);
-void        npd_draw_model                    (NPDModel   *model,
-                                               NPDDisplay *display);
+                                               gint        position_y,
+                                               gint        square_size);
+void        npd_draw_model_into_image         (NPDModel   *model,
+                                               NPDImage   *image);
 void        npd_draw_mesh                     (NPDModel   *model,
                                                NPDDisplay *display);
-
-void        npd_destroy_image                 (NPDImage   *image);
-
-void        npd_texture_fill_triangle         (gint        x1,
-                                               gint        y1,
-                                               gint        x2,
-                                               gint        y2,
-                                               gint        x3,
-                                               gint        y3,
-                                               NPDMatrix  *A,
-                                               NPDImage   *input_image,
-                                               NPDImage   *output_image,
-                                               NPDSettings settings);
-void        npd_texture_quadrilateral         (NPDBone    *reference_bone,
-                                               NPDBone    *current_bone,
-                                               NPDImage   *input_image,
-                                               NPDImage   *output_image,
-                                               NPDSettings settings);
-void        npd_draw_texture_line             (gint        x1,
-                                               gint        x2,
-                                               gint        y,
-                                               NPDMatrix  *A,
-                                               NPDImage   *input_image,
-                                               NPDImage   *output_image,
-                                               NPDSettings settings);
+gboolean    npd_is_color_transparent          (NPDColor *color);
 void      (*npd_draw_line)                    (NPDDisplay *display,
                                                gfloat      x0,
                                                gfloat      y0,
                                                gfloat      x1,
                                                gfloat      y1);
-gfloat      npd_bilinear_interpolation        (gfloat      I0,
-                                               gfloat      I1,
-                                               gfloat      I2,
-                                               gfloat      I3,
-                                               gfloat      dx,
-                                               gfloat      dy);
-void        npd_bilinear_color_interpolation  (NPDColor   *I0,
-                                               NPDColor   *I1,
-                                               NPDColor   *I2,
-                                               NPDColor   *I3,
-                                               gfloat      dx,
-                                               gfloat      dy,
-                                               NPDColor   *out);
-gfloat      npd_blend_band                    (gfloat      src,
-                                               gfloat      dst,
-                                               gfloat      src_alpha,
-                                               gfloat      dest_alpha,
-                                               gfloat      out_alpha);
-void        npd_blend_colors                 (NPDColor   *src,
-                                               NPDColor   *dst,
-                                               NPDColor   *out_color);
 void      (*npd_get_pixel_color)              (NPDImage   *image,
                                                gint        x,
                                                gint        y,
@@ -117,10 +71,5 @@ void      (*npd_set_pixel_color)              (NPDImage   *image,
                                                gint        x,
                                                gint        y,
                                                NPDColor   *color);
-gboolean    npd_compare_colors                (NPDColor   *c1,
-                                               NPDColor   *c2);
-gboolean    npd_is_color_transparent          (NPDColor   *color);
-gboolean    npd_init_display                  (NPDDisplay *display);
-void        npd_destroy_display               (NPDDisplay *display);
 
 #endif	/*__NPD_GRAPHICS_H__ */
