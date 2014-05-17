f12750ad4af7eef86068bddd20218cd9a93bbe5d
Øyvind Kolås <pippin@gimp.org>
2014-05-17 14:07:29 +0200

    svg-saturate: use gegl-op.h

--- operations/common/svg-saturate.c
+++ operations/common/svg-saturate.c
@@ -20,17 +20,18 @@
 #include "config.h"
 #include <glib/gi18n-lib.h>
 
+#ifdef GEGL_PROPERTIES
 
-#ifdef GEGL_CHANT_PROPERTIES
-
-gegl_chant_string (values, _("Values"), "", _("list of <number>s"))
+gegl_property_string (values, "nick", _("Values"),
+    "blurb", _("list of <number>s"),
+    NULL)
 
 #else
 
-#define GEGL_CHANT_TYPE_POINT_FILTER
-#define GEGL_CHANT_C_FILE          "svg-saturate.c"
+#define GEGL_OP_POINT_FILTER
+#define GEGL_OP_C_FILE          "svg-saturate.c"
 
-#include "gegl-chant.h"
+#include "gegl-op.h"
 #include <math.h>
 #include <stdlib.h>
 
@@ -51,7 +52,7 @@ process (GeglOperation       *op,
          const GeglRectangle *roi,
          gint                 level)
 {
-  GeglChantO *o = GEGL_CHANT_PROPERTIES (op);
+  GeglProperties *o = GEGL_PROPERTIES (op);
   gfloat     *in = in_buf;
   gfloat     *out = out_buf;
   gfloat     *m;
@@ -110,7 +111,7 @@ process (GeglOperation       *op,
 
 
 static void
-gegl_chant_class_init (GeglChantClass *klass)
+gegl_op_class_init (GeglOpClass *klass)
 {
   GeglOperationClass            *operation_class;
   GeglOperationPointFilterClass *point_filter_class;
