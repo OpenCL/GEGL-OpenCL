e3d0e3515bf8e383021037fc3badf2119cc4c2ee
Øyvind Kolås <pippin@gimp.org>
2014-05-23 01:18:58 +0200

    exposure: restore sane default gamma value

--- operations/common/exposure.c
+++ operations/common/exposure.c
@@ -30,7 +30,7 @@ property_double (offset, _("Offset"), 0.0)
     description (_("Offset value added"))
     value_range (-0.5, 0.5)
 
-property_double (gamma, _("Gamma correction"), 0.01)
+property_double (gamma, _("Gamma correction"), 1.0)
     value_range (0.01, 10)
     ui_range    (0.01, 3.0)
 
