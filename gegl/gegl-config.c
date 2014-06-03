dd64bfcfdce168fbddb38752ad4e0d6206e44948
Øyvind Kolås <pippin@gimp.org>
2014-06-04 00:22:55 +0200

    buffer: change default tile size to 128x256
    For a three layer large GIMP test document, the debug benchmark of
    revalidating the projection goes from 1.65 seconds to 1.14 seconds after the
    change.

--- gegl/gegl-config.c
+++ gegl/gegl-config.c
@@ -189,7 +189,7 @@ gegl_config_class_init (GeglConfigClass *klass)
                                    g_param_spec_int ("tile-height",
                                                      "Tile height",
                                                      "default tile height for created buffers.",
-                                                     0, G_MAXINT, 64,
+                                                     0, G_MAXINT, 256,
                                                      G_PARAM_READWRITE |
                                                      G_PARAM_CONSTRUCT));
 
