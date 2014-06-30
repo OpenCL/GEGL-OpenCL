6e50706d0b918cdce383a95f0f9e8090f149dec5
Øyvind Kolås <pippin@gimp.org>
2014-06-30 03:27:59 +0200

    buffer: set default tile size to 256x128

--- gegl/gegl-config.c
+++ gegl/gegl-config.c
@@ -181,7 +181,7 @@ gegl_config_class_init (GeglConfigClass *klass)
                                    g_param_spec_int ("tile-width",
                                                      "Tile width",
                                                      "default tile width for created buffers.",
-                                                     0, G_MAXINT, 128,
+                                                     0, G_MAXINT, 256,
                                                      G_PARAM_READWRITE |
                                                      G_PARAM_CONSTRUCT));
 
