7018e97ad1aa1b1c6ae7486d27a2b5d939687a51
Marco Ciampa <ciampix@libero.it>
2015-04-27 08:30:13 +0200

    Escaped percent sign in gettext string.

--- operations/common/brightness-contrast.c
+++ operations/common/brightness-contrast.c
@@ -149,7 +149,7 @@ gegl_op_class_init (GeglOpClass *klass)
       "name",       "gegl:brightness-contrast",
       "title",      "Brightness Contrast",
       "categories", "color",
-      "description", _("Changes the light level and contrast. This operation operates in linear light, 'contrast' is a scale factor around 50% gray, and 'brightness' a constant offset to apply after contrast scaling."),
+      "description", _("Changes the light level and contrast. This operation operates in linear light, 'contrast' is a scale factor around 50%% gray, and 'brightness' a constant offset to apply after contrast scaling."),
       "cl-source"  , brightness_contrast_cl_source,
       "reference-composition", composition,
       NULL);
