cfabbd6dfd0c6b0c5f84d99a75ba5a717f6ab584
Thomas Manni <thomas.manni@free.fr>
2015-06-24 19:54:56 +0200

    illusion: opt out of multithreading

--- operations/common/illusion.c
+++ operations/common/illusion.c
@@ -237,6 +237,7 @@ gegl_op_class_init (GeglOpClass *klass)
   operation_class->get_required_for_output = get_required_for_output;
   operation_class->get_cached_region       = get_cached_region;
   operation_class->opencl_support          = FALSE;
+  operation_class->threaded                = FALSE;
 
   gegl_operation_class_set_keys (operation_class,
       "name",        "gegl:illusion",
