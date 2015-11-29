23051417f7c525d9ba51f0afedf605347e73759c
Michael Natterer <mitch@gimp.org>
2015-11-29 21:44:35 +0100

    operations: no dots after op descriptions

--- operations/common/gaussian-blur-selective.c
+++ operations/common/gaussian-blur-selective.c
@@ -259,7 +259,7 @@ gegl_op_class_init (GeglOpClass *klass)
    "title",       _("Selective Gaussian Blur"),
    "categories",  "enhance:noise-reduction",
    "license",     "GPL3+",
-   "description", _("Blur neighboring pixels, but only in low-contrast areas."),
+   "description", _("Blur neighboring pixels, but only in low-contrast areas"),
    NULL);
 }
 
