a185a9370aae8dd83d5cc7999ae4334b5aab50d6
Øyvind Kolås <pippin@gimp.org>
2016-03-05 13:38:52 +0100

    gegl-binary: document property assignment

--- bin/gegl.c
+++ bin/gegl.c
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, see <http://www.gnu.org/licenses/>.
  *
- * Copyright (C) 2003, 2004, 2006, 2007, 2008 Øyvind Kolås
+ * Copyright (C) 2003, 2004, 2006, 2007, 2008, 2016 Øyvind Kolås
  */
 
 #include "config.h"
