c1ff6484a3e8487c1be0c4cf7f7a6b3cb60a4d27
Jehan <jehan@girinstud.io>
2016-09-26 18:09:09 +0200

    operations, tools: module.c not properly generated in VPATH builds.
    None of the core, common and generated operations were registered inside
    respective `common.c` when `gen-loader.sh` was not run from inside the
    directory with all the source files. Such build case typically happens
    on VPATH builds.

--- tools/gen-loader.sh
+++ tools/gen-loader.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 echo '#include "config.h"'
-echo '#include <gegl-plugin.h>' 
+echo '#include <gegl-plugin.h>'
 
-for a in `grep GEGL_OP_NAME *.c | sed 's/.*NAME//'`;do echo "void gegl_op_"$a"_register_type(GTypeModule *module);" ;done
+for a in `grep GEGL_OP_NAME "$@" | sed 's/.*NAME//'`;do echo "void gegl_op_"$a"_register_type(GTypeModule *module);" ;done
 
-echo 'static const GeglModuleInfo modinfo = { 
+echo 'static const GeglModuleInfo modinfo = {
 GEGL_MODULE_ABI_VERSION
 };
 
@@ -23,7 +23,7 @@ gegl_module_register (GTypeModule *module)
 {'
 
 
-for a in `grep GEGL_OP_NAME *.c | sed 's/.*NAME//'`;do echo "  gegl_op_"$a"_register_type(module);" ;done
+for a in `grep GEGL_OP_NAME "$@" | sed 's/.*NAME//'`;do echo "  gegl_op_"$a"_register_type(module);" ;done
 
 
  echo ' return TRUE;
