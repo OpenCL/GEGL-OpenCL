7e3816708bf7650c7204d1db7d2ef555d908667e
Øyvind Kolås <pippin@gimp.org>
2017-01-30 23:09:34 +0100

    ops/crop; remove accidental stdio.h include

--- operations/core/crop.c
+++ operations/core/crop.c
@@ -52,7 +52,6 @@ property_boolean (reset_origin, _("Reset origin"), FALSE)
 
 #include "gegl-op.h"
 #include <math.h>
-#include <stdio.h>
 
 static void
 gegl_crop_prepare (GeglOperation *operation)
