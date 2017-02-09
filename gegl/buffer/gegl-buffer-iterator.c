OpenCL/GEGL-OpenCL

commit 3da03808a3e47ad8ec50e41ed53f3785d7217361

buffer: make mipmap tests pass by default
a subset of operations might now be able to have mipmap rendering accelerated
previews.

gegl/buffer/gegl-buffer-iterator.c
@@ -122,10 +122,22 @@ gegl_buffer_iterator_add (GeglBufferIterator  *iter,
                           GeglAccessMode       access_mode,
                           GeglAbyssPolicy      abyss_policy)
 {
+  GeglRectangle roi2;
   GeglBufferIteratorPriv *priv = iter->priv;
   int                     index;
   SubIterState           *sub;
-  level = 0;
+
+  //level = 0; // XXX - this disables mipmapping for iteration while it
+             //       is known to be broken
+
+  if (level)
+  {
+    roi2.x = roi->x >> level;
+    roi2.y = roi->y >> level;
+    roi2.width = roi->width >> level;
+    roi2.height  = roi->height >> level;
+    roi = &roi2;
+  }
 
   g_return_val_if_fail (priv->num_buffers < GEGL_BUFFER_MAX_ITERATORS, 0);
 
@@ -188,8 +200,13 @@ release_tile (GeglBufferIterator *iter,
     {
       if (sub->access_mode & GEGL_ACCESS_WRITE)
         {
+          GeglRectangle roi = {sub->real_roi.x << sub->level,
+                               sub->real_roi.y << sub->level,
+                               sub->real_roi.width << sub->level,
+                               sub->real_roi.height << sub->level};
+
           gegl_buffer_set_unlocked_no_notify (sub->buffer,
-                                              &sub->real_roi,
+                                              &roi,
                                               sub->level,
                                               sub->format,
                                               sub->real_data,
@@ -220,6 +237,7 @@ retile_subs (GeglBufferIterator *iter,
   GeglBufferIteratorPriv *priv = iter->priv;
   GeglRectangle real_roi;
   int index;
+  int level = priv->sub_iter[0].level;
 
   int shift_x = priv->origin_tile.x;
   int shift_y = priv->origin_tile.y;
@@ -317,8 +335,8 @@ get_tile (GeglBufferIterator *iter,
       int tile_width  = buf->tile_width;
       int tile_height = buf->tile_height;
 
-      int tile_x = gegl_tile_indice (iter->roi[index].x + shift_x, tile_width, sub->level);
-      int tile_y = gegl_tile_indice (iter->roi[index].y + shift_y, tile_height, sub->level);
+      int tile_x = gegl_tile_indice (iter->roi[index].x + shift_x, tile_width, 0);
+      int tile_y = gegl_tile_indice (iter->roi[index].y + shift_y, tile_height, 0);
 
       sub->current_tile = gegl_buffer_get_tile (buf, tile_x, tile_y, sub->level);
 
@@ -371,7 +389,7 @@ needs_indirect_read (GeglBufferIterator *iter,
 {
   GeglBufferIteratorPriv *priv = iter->priv;
   SubIterState           *sub  = &priv->sub_iter[index];
-
+  
   if (sub->access_mode & GEGL_ITERATOR_INCOMPATIBLE)
     return TRUE;
 

gegl/buffer/gegl-buffer-access.c
@@ -399,8 +399,8 @@ gegl_buffer_iterate_write (GeglBuffer          *buffer,
           else
             pixels = tile_width - offsetx;
 
-          index_x = gegl_tile_indice (tiledx, tile_width, level);
-          index_y = gegl_tile_indice (tiledy, tile_height, level);
+          index_x = gegl_tile_indice (tiledx, tile_width, 0);
+          index_y = gegl_tile_indice (tiledy, tile_height, 0);
 
           tile = gegl_buffer_get_tile (buffer, index_x, index_y, level);
 
@@ -968,8 +968,8 @@ gegl_buffer_iterate_read_abyss_loop (GeglBuffer          *buffer,
   gint          origin_x;
 
   /* Loop abyss works like iterating over a grid of tiles the size of the abyss */
-  gint loop_chunk_ix = gegl_tile_indice (roi->x - abyss->x, abyss->width, level);
-  gint loop_chunk_iy = gegl_tile_indice (roi->y - abyss->y, abyss->height, level);
+  gint loop_chunk_ix = gegl_tile_indice (roi->x - abyss->x, abyss->width, 0);
+  gint loop_chunk_iy = gegl_tile_indice (roi->y - abyss->y, abyss->height, 0);
 
   current_roi.x = loop_chunk_ix * abyss->width  + abyss->x;
   current_roi.y = loop_chunk_iy * abyss->height + abyss->y;

tests/buffer/reference/mipmap_iterator.buf
@@ -48,12 +48,12 @@ Test: mipmap_iterator
 ▌                    ▐
 ▌                    ▐
 ▌░░░░░░░░░░░░░░░░░░░░▐
-▌░░░░    ░░░░░░░░░░░░▐
-▌░░░░    ░░░░░░░░░░░░▐
-▌░░░░    ░░░░░░░░░░░░▐
-▌░░░░    ░░░░░░░░░░░░▐
-▌▒▒▒▒    ▒▒▒▒▒▒▒▒▒▒▒▒▐
-▌▒▒▒▒    ▒▒▒▒▒▒▒▒▒▒▒▒▐
+▌░░░░░░░░░░░░░░░░░░░░▐
+▌░░░░░░░░░░░░░░░░░░░░▐
+▌░░░░░░░░░░░░░░░░░░░░▐
+▌░░░░░░░░░░░░░░░░░░░░▐
+▌▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▐
+▌▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▐
 ▌▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▐
 ▌▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▐
 ▌▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▐

tests/buffer/reference/mipmap_iterator2.buf
@@ -4,17 +4,17 @@ Test: mipmap_iterator2
 ▌                    ▐
 ▌                    ▐
 ▌░░░░░░░░░░░░░░░░░░░░▐
-▌░░░░▓▓▓▓▓▓▓▓▓░░░░░░░▐
-▌░░░░▓▓▓▓▓▓▓▓▓░░░░░░░▐
-▌░░░░▓▓▓▓▓▓▓▓▓░░░░░░░▐
-▌░░░░▓▓▓▓▓▓▓▓▓░░░░░░░▐
-▌▒▒▒▒█████████▒▒▒▒▒▒▒▐
-▌▒▒▒▒█████████▒▒▒▒▒▒▒▐
-▌▒▒▒▒█████████▒▒▒▒▒▒▒▐
-▌▒▒▒▒█████████▒▒▒▒▒▒▒▐
-▌▒▒▒▒█████████▒▒▒▒▒▒▒▐
-▌▓▓▓▓█████████▓▓▓▓▓▓▓▐
-▌▓▓▓▓█████████▓▓▓▓▓▓▓▐
+▌░░░░░░░░░░░░░░░░░░░░▐
+▌░░░░░░░░░░░░░░░░░░░░▐
+▌░░░░░░░░░░░░░░░░░░░░▐
+▌░░░░░░░░░░░░░░░░░░░░▐
+▌▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▐
+▌▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▐
+▌▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▐
+▌▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▐
+▌▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▐
+▌▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▐
+▌▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▐
 ▌▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▐
 ▌▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▐
 ▌▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▐
@@ -24,12 +24,12 @@ Test: mipmap_iterator2
 ▛▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▜
 ▌                    ▐
 ▌░░░░░░░░░░          ▐
-▌░░▓▓▓▓▒░░░          ▐
-▌░░▓▓▓▓▒░░░          ▐
-▌▒▒████▓▒▒▒          ▐
-▌▒▒████▓▒▒▒          ▐
-▌▓▓█████▓▓▓          ▐
+▌░░▓▓▓▓░░░░          ▐
+▌░░▓▓▓▓░░░░          ▐
+▌▒▒████▒▒▒▒          ▐
+▌▒▒████▒▒▒▒          ▐
 ▌▓▓████▓▓▓▓          ▐
+▌▓▓▓▓▓▓▓▓▓▓          ▐
 ▌▓▓▓▓▓▓▓▓▓▓          ▐
 ▌██████████          ▐
 ▌                    ▐

tests/buffer/reference/mipmap_sampler.buf
@@ -1,31 +1,31 @@
 Test: mipmap_sampler
 ▛▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▜
-▌  ░░░░▒▒▒▒▓▓▓▓██    ▐
-▌  ░░░░▒▒▒▒▓▓▓▓██    ▐
-▌  ░░░░▒▒▒▒▓▓▓▓██    ▐
-▌  ░░░░▒▒▒▒▓▓▓▓██    ▐
-▌  ░░░░▒▒▒▒▓▓▓▓██    ▐
-▌  ░░░░▒▒▒▒▓▓▓▓██    ▐
-▌  ░░░░▒▒▒▒▓▓▓▓██    ▐
-▌  ░░░░▒▒▒▒▓▓▓▓██    ▐
-▌  ░░░░▒▒▒▒▓▓▓▓██    ▐
-▌  ░░░░▒▒▒▒▓▓▓▓██    ▐
-▌  ░░░░▒▒▒▒▓▓▓▓██    ▐
-▌  ░░░░▒▒▒▒▓▓▓▓██    ▐
-▌  ░░░░▒▒▒▒▓▓▓▓██    ▐
-▌  ░░░░▒▒▒▒▓▓▓▓██    ▐
-▌  ░░░░▒▒▒▒▓▓▓▓██    ▐
-▌  ░░░░▒▒▒▒▓▓▓▓██    ▐
+▌                    ▐
+▌                    ▐
+▌                    ▐
+▌                    ▐
+▌                    ▐
+▌                    ▐
+▌                    ▐
+▌                    ▐
+▌                    ▐
+▌                    ▐
+▌                    ▐
+▌                    ▐
+▌                    ▐
+▌                    ▐
+▌                    ▐
+▌                    ▐
 ▌                    ▐
 ▌                    ▐
 ▌                    ▐
 ▌                    ▐
 ▙▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▟
-4.000000,5.000000 : 0.281250
-6.000000,4.000000 : 0.406250
-8.000000,10.000000 : 0.531250
+4.000000,5.000000 : 0.250000
+6.000000,4.000000 : 0.375000
+8.000000,10.000000 : 0.500000
 ▛▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▜
-▌ ░░▒▒▓▓█            ▐
+▌                    ▐
 ▌ ░░▒▒▓▓█            ▐
 ▌ ░░▒▒▓▓█            ▐
 ▌ ░░▒▒▓▓█            ▐

tests/mipmap/invert-crop.sh
@@ -8,7 +8,7 @@ if [ ! -f $abs_top_builddir/bin/gegl ]; then
   echo "Skipping test-invert-crop due to lack of gegl executable"
   exit 77
 else
-  GEGL_PATH=$abs_top_builddir/operations GEGL_USE_OPENCL=no GEGL_MIPMAP_RENDERING=1 $abs_top_builddir/bin/gegl                                       \
+  GEGL_TILE_SIZE=8x8 GEGL_PATH=$abs_top_builddir/operations GEGL_USE_OPENCL=no GEGL_MIPMAP_RENDERING=1 $abs_top_builddir/bin/gegl                                       \
           -s 0.33 $abs_top_srcdir/tests/compositions/data/car-stack.png -o      \
           $abs_top_builddir/tests/mipmap/invert-crop-output.png           \
           -- invert crop x=147 y=66 width=200 height=200 \

commit 0d72a2d7947c496aec0368ce16a5642c7c67118b

buffer-iterator: Silence -Wunused-variable
Fallout from 3da03808a3e47ad8ec50e41ed53f3785d7217361

gegl/buffer/gegl-buffer-iterator.c
@@ -237,7 +237,6 @@ retile_subs (GeglBufferIterator *iter,
   GeglBufferIteratorPriv *priv = iter->priv;
   GeglRectangle real_roi;
   int index;
-  int level = priv->sub_iter[0].level;
 
   int shift_x = priv->origin_tile.x;
   int shift_y = priv->origin_tile.y;