f341df2dc9c27223c5b9ba9c092902812fceb476
Øyvind Kolås <pippin@gimp.org>
2017-02-26 22:47:59 +0100

    noise-perlin: remove unused seed property

--- operations/common/noise-perlin.c
+++ operations/common/noise-perlin.c
@@ -33,10 +33,6 @@ property_double (zoff,  _("Z offset"), -1)
 property_int (n, _("Iterations"), 3)
   value_range     (0, 20)
 
-  /* XXX: what? */
-property_double (seed, _("Random seed"), 0.0)
-  ui_range (0.0, 100.0)
-
 #else
 
 #define GEGL_OP_POINT_RENDER
