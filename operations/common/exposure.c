91d0febb63b2172ddb627dba7fea710d89f3c3f3
Øyvind Kolås <pippin@gimp.org>
2017-03-02 13:45:48 +0100

    exposure: bug 754073 fix opencl kernel making it compile

--- operations/common/exposure.c
+++ operations/common/exposure.c
@@ -105,7 +105,7 @@ static const char* kernel_source =
 "  int gid = get_global_id(0);                                     \n"
 "  float4 in_v  = in[gid];                                         \n"
 "  float4 out_v;                                                   \n"
-"  out_v.xyz =  ((in_v.xyz - black_level) * gain)                  \n"
+"  out_v.xyz =  ((in_v.xyz - black_level) * gain);                 \n"
 "  out_v.w   =  in_v.w;                                            \n"
 "  out[gid]  =  out_v;                                             \n"
 "}                                                                 \n";
