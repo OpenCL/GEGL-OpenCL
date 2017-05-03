415ebc0ef04553101d8d597d2b0309c8822d2b7d
Øyvind Kolås <pippin@gimp.org>
2017-05-03 02:30:40 +0200

    exposure: do a 1.5stop overexposure as example image

--- operations/common/exposure.c
+++ operations/common/exposure.c
@@ -180,6 +180,7 @@ gegl_op_class_init (GeglOpClass *klass)
     "name",        "gegl:exposure",
     "title",       _("Exposure"),
     "categories",  "color",
+    "reference-chain", "load path=images/standard-input.png exposure exposure=1.5",
     "description", _("Changes Exposure of an image, allows stepping HDR and photographs up/down in stops. "),
     "op-version",  "1:0",
     NULL);
